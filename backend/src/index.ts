f75b88d8e55bac1b76db48bde7f066f2153db9ec
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-22 16:03:35 +0100

    feat: use inside index the router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { userRouter } from './router';
+import mongoose from "mongoose"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const URL: string = process.env.DB_URL || "";
+mongoose.connect(URL);
 
 app.use(express.json());
 
 app.get('/', (_, res) => {
     res.send(`Running in ${process.env.NODE_ENV} mode`);
 });
+
+app.use("/user", userRouter);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
