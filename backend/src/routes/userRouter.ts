52e718a3a055a5db60b078a00569d4d1b8002d9a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-22 17:34:59 +0100

    feat: link delete user handler

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, checkToken } from "../controllers/userController";
+import { registerUser, loginUser, checkToken, deleteUser } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -13,5 +13,8 @@ userRouter.route("/login")
 
 userRouter.route("/authorized")
     .post(checkToken);
+
+userRouter.route("/delete")
+    .delete(deleteUser);
 
 export { userRouter }
