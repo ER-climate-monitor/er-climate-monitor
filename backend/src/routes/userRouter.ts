5d83161a0f68411956abd475f501d7a1c568c13f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 14:44:12 +0100

    feat: add routes for the admin

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, checkToken, deleteUser } from "../controllers/userController";
+import { registerUser, loginUser, checkToken, deleteUser, registerAdmin, loginAdmin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -8,8 +8,13 @@ const userRouter = express.Router();
 userRouter.route("/register")
     .post(registerUser);
 
+userRouter.route("/admin/register")
+    .post(registerAdmin)
+
 userRouter.route("/login")
     .post(loginUser);
+userRouter.route("/admin/login")
+    .post(loginAdmin)
 
 userRouter.route("/authorized")
     .post(checkToken);
