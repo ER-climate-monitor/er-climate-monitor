f31973af7e9739bbb20819aa9ef8b75a0082c7f2
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 14:43:39 +0100

    feat: add the create user function

--- backend/src/controllers/utils/userUtils.ts
+++ backend/src/controllers/utils/userUtils.ts
@@ -1,8 +1,19 @@
-import { userModel } from "../../models/userModel";
+import { UserDocument, userModel } from "../../models/userModel";
+import { saltRounds } from "../userController";
+import bcrypt from "bcrypt";
+
 
 async function checkUser(inputEmail: String): Promise<Boolean> {
     const existingUser: Boolean = await userModel.findOne({email: inputEmail}) || false;
     return existingUser
 }
 
-export { checkUser }
+async function createUser(inputEmail: string, password: string, role: string): Promise<UserDocument> {
+    const hash: string = await bcrypt.hash(password, saltRounds);
+    const newUser: UserDocument = new userModel({email: inputEmail, password: hash, role: role});
+    newUser.save();
+    return newUser
+}
+
+
+export { checkUser, createUser }
