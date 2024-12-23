b5c6b34a1c18398ccb6081ec7735605b9ea372da
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 16:23:06 +0100

    test: add user registration

--- backend/src/test/globals/user.test.ts
+++ backend/src/test/globals/user.test.ts
@@ -1,11 +1,44 @@
-import chai from "chai"
-import chaiHttp from "chai-http";
+import { expect } from "chai"
 import request from "supertest"
-import { app } from "../..";
-import { describe } from "test";
+import createServer from "../..";
+import { describe, it, before } from "node:test";
+import HttpStatus from "http-status-codes";
+import { USER_EMAIL_HEADER, USER_PASSWORD_HEADER } from "../../controllers/userController";
+import { Application, response } from "express";
 
-chai.use(chaiHttp);
+const TEST_PORT = 10_000;
 
-describe("API Tests", () => {
-
+const app: Application = createServer();
+const server = app.listen(TEST_PORT, () => { 
+    console.log("Server listening on port: ", TEST_PORT);
+    app.emit("ready");
 });
+
+async function deleteUser(email: string, password: string) {
+    const userInformation = {
+        [USER_EMAIL_HEADER]: email,
+        [USER_PASSWORD_HEADER]: password
+    }
+    const resopnse = (await request(app).delete("/user/delete").send(userInformation));
+    expect(response.statusCode).to.equal(HttpStatus.OK);
+}
+
+
+
+app.on("ready", () => {
+    describe("New User Registration", async () => {
+        await it("should return OK if the email does not exists inside the Database", async () => {
+            const email = "testemail1@gmail.com";
+            const password = "AVeryStrongPassword1010";
+            const userInformation = {
+                [USER_EMAIL_HEADER]: email,
+                [USER_PASSWORD_HEADER]: password
+            }
+            console.log(userInformation)
+            const response = (await request(app).post("/user/register").send(userInformation));
+            await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+            await deleteUser(email, password);
+        });
+    });
+});
+
