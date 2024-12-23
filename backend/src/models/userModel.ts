c2098208c5c927ddce8acb0e058a9cb063a562bd
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 14:44:19 +0100

    feat: add role inside the user schema

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -3,6 +3,7 @@ import mongoose, { Model, Document } from "mongoose"
 interface IUser {
     email: string;
     password: string;
+    role: string;
 }
 
 interface UserDocument extends IUser, Document {}
@@ -11,6 +12,7 @@ interface UserDocument extends IUser, Document {}
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: {type: String, required: true}
 });
 
 const userModel: Model<UserDocument> = mongoose.model<UserDocument>("Users", userSchema);
