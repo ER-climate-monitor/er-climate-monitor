35b99ab2a134f4ac853a90fa8b89b82ee1911da2
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 17:07:05 +0100

    test: add test for creating a new user with an existing email

--- backend/src/test/globals/user.test.ts
+++ backend/src/test/globals/user.test.ts
@@ -1,12 +1,13 @@
 import { expect } from "chai"
 import request from "supertest"
 import createServer from "../..";
-import { describe, it, before, after } from "node:test";
+import { describe, it, after } from "node:test";
 import HttpStatus from "http-status-codes";
 import { USER_EMAIL_HEADER, USER_PASSWORD_HEADER } from "../../controllers/userController";
 import { Application, response } from "express";
 
 const TEST_PORT = 10_000;
+console.log(process.env);
 
 const app: Application = createServer();
 const server = app.listen(TEST_PORT, () => { 
@@ -24,8 +25,8 @@ async function deleteUser(email: string, password: string) {
 }
 
 app.on("ready", () => {
-    describe("New User Registration", () => {
-        it("should return OK if the email does not exists inside the Database", async () => {
+    describe("New User Registration", async () => {
+        await it("should return OK if the email does not exists inside the Database", async () => {
             const email = "testemail1@gmail.com";
             const password = "AVeryStrongPassword1010";
             const userInformation = {
@@ -34,10 +35,23 @@ app.on("ready", () => {
             }
             const response = (await request(app).post("/user/register").send(userInformation));
             await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+            await expect(response.headers[USER_EMAIL_HEADER.toLowerCase()]).to.equal(userInformation[USER_EMAIL_HEADER]);
+            await deleteUser(email, password);
+        });
+        await it("should return an error if I try to create a new user with an email already registered", async () => {
+            const email = "testemail1@gmail.com";
+            const password = "AVeryStrongPassword1010";
+            const userInformation = {
+                [USER_EMAIL_HEADER]: email,
+                [USER_PASSWORD_HEADER]: password
+            };
+            const response = (await request(app).post("/user/register").send(userInformation));
+            const secondResponse = await request(app).post("/user/register").send(userInformation);
+            await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+            await expect(secondResponse.statusCode).to.equal(HttpStatus.CONFLICT);
             await deleteUser(email, password);
         });
         after(() => {
-            console.log("done")
             server.close(() => {
                 console.log("Closing the server...");
                 process.exit(0);
