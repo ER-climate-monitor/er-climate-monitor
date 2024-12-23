278dd833b85fd3cade9cea7855162553f7d9e49b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-23 15:07:03 +0100

    feat: init tests

---
+++ backend/src/test/globals/user.test.ts
@@ -0,0 +1,11 @@
+import chai from "chai"
+import chaiHttp from "chai-http";
+import request from "supertest"
+import { app } from "../..";
+import { describe } from "test";
+
+chai.use(chaiHttp);
+
+describe("API Tests", () => {
+
+});
