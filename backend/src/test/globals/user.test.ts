f2782b542e2dbc5c6b9720e231e0d63526944766
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-24 13:03:10 +0100

    test: use mocha for unit testing

--- backend/src/test/globals/user.test.ts
+++ backend/src/test/globals/user.test.ts
@@ -1,13 +1,13 @@
 import { expect } from "chai"
 import request from "supertest"
 import createServer from "../..";
-import { describe, it, after } from "node:test";
+import { describe, it, after, before } from "mocha";
 import HttpStatus from "http-status-codes";
 import { USER_EMAIL_HEADER, USER_PASSWORD_HEADER } from "../../controllers/userController";
 import { Application, response } from "express";
 
 const TEST_PORT = 10_000;
-console.log(process.env);
+const TWO_SECOND = 2_000;
 
 const app: Application = createServer();
 const server = app.listen(TEST_PORT, () => { 
@@ -24,38 +24,54 @@ async function deleteUser(email: string, password: string) {
     expect(response.statusCode).to.equal(HttpStatus.OK);
 }
 
-app.on("ready", () => {
-    describe("New User Registration", async () => {
-        await it("should return OK if the email does not exists inside the Database", async () => {
-            const email = "testemail1@gmail.com";
-            const password = "AVeryStrongPassword1010";
-            const userInformation = {
-                [USER_EMAIL_HEADER]: email,
-                [USER_PASSWORD_HEADER]: password
-            }
-            const response = (await request(app).post("/user/register").send(userInformation));
-            await expect(response.statusCode).to.equal(HttpStatus.CREATED);
-            await expect(response.headers[USER_EMAIL_HEADER.toLowerCase()]).to.equal(userInformation[USER_EMAIL_HEADER]);
-            await deleteUser(email, password);
-        });
-        await it("should return an error if I try to create a new user with an email already registered", async () => {
-            const email = "testemail1@gmail.com";
-            const password = "AVeryStrongPassword1010";
-            const userInformation = {
-                [USER_EMAIL_HEADER]: email,
-                [USER_PASSWORD_HEADER]: password
-            };
-            const response = (await request(app).post("/user/register").send(userInformation));
-            const secondResponse = await request(app).post("/user/register").send(userInformation);
-            await expect(response.statusCode).to.equal(HttpStatus.CREATED);
-            await expect(secondResponse.statusCode).to.equal(HttpStatus.CONFLICT);
-            await deleteUser(email, password);
-        });
-        after(() => {
-            server.close(() => {
-                console.log("Closing the server...");
-                process.exit(0);
-            });
-        })
+async function sleep(ms: number): Promise<any> {
+    return new Promise((resolve) => { setTimeout(resolve, ms); });
+}
+
+describe("New User Registration", () => {
+    it("should return OK if the email does not exists inside the Database", async () => {
+        const email = "testemail1@gmail.com";
+        const password = "AVeryStrongPassword1010";
+        const userInformation = {
+            [USER_EMAIL_HEADER]: email,
+            [USER_PASSWORD_HEADER]: password
+        }
+        const response = (await request(app).post("/user/register").send(userInformation));
+        await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+        await expect(response.headers[USER_EMAIL_HEADER.toLowerCase()]).to.equal(userInformation[USER_EMAIL_HEADER]);
+        await deleteUser(email, password);
     });
+    it("should return an error if I try to create a new user with an email already registered", async () => {
+        const email = "testemail1@gmail.com";
+        const password = "AVeryStrongPassword1010";
+        const userInformation = {
+            [USER_EMAIL_HEADER]: email,
+            [USER_PASSWORD_HEADER]: password
+        };
+        const response = (await request(app).post("/user/register").send(userInformation));
+        const secondResponse = await request(app).post("/user/register").send(userInformation);
+        await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+        await expect(secondResponse.statusCode).to.equal(HttpStatus.CONFLICT);
+        await deleteUser(email, password);
+    });
+    it("should return OK if I register an adming using the correct API key and using an email that does not exist", async () => {
+        const email = "testemail1@gmail.com";
+        const password = "AVeryStrongPassword1010";
+        const api_key = process.env.SECRET_API_KEY || "";
+        const userInformation = {
+            [USER_EMAIL_HEADER]: email,
+            [USER_PASSWORD_HEADER]: password,
+            [String(process.env.API_KEY_HEADER)]: api_key
+        };
+        const response = (await request(app).post("/user/admin/register").send(userInformation));
+        await expect(response.statusCode).to.equal(HttpStatus.CREATED);
+        await expect(response.headers[USER_EMAIL_HEADER.toLowerCase()]).to.equal(userInformation[USER_EMAIL_HEADER]);
+        await deleteUser(email, password)
+    });
+    after(() => {
+        server.close(() => {
+            console.log("Closing the server...");
+            process.exit(0);
+        });
+    })
 });
