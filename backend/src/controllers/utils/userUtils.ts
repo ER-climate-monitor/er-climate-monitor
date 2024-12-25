15575b1e0572b6fae76de9f4388b54769afa884c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-25 12:17:04 +0100

    feat: add regex for email

--- backend/src/controllers/utils/userUtils.ts
+++ backend/src/controllers/utils/userUtils.ts
@@ -19,5 +19,10 @@ async function deleteOneUser(email:string): Promise<boolean> {
     return status.deletedCount === 1
 }
 
+function checkEmail(email: string): boolean { 
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    return emailRegex.test(email);
+}
 
-export { checkUser, createUser, deleteOneUser }
+
+export { checkUser, createUser, deleteOneUser, checkEmail }
