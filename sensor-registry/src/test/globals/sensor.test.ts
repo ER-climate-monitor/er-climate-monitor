991861fd93ee4a2ec912d1ada3751da5f7deb42f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-26 16:50:17 +0100

    refactor: change dir name

---
+++ sensor-registry/src/test/globals/sensor.test.ts
@@ -0,0 +1,11 @@
+import { describe } from "mocha"
+import createServer from "../..";
+
+
+const app = createServer();
+
+describe("Registering a new Sensor", () => {
+    it("Registering a new Sensor that does not exists inside the database should be OK", async () =>{
+
+    });
+});
