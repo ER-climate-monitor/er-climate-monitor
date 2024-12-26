dd9a9df746389635dffaee1659168a13a4dbb79d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-26 16:49:58 +0100

    feat: export function for creating new express server

--- sensor-registry/src/index.ts
+++ sensor-registry/src/index.ts
@@ -1,18 +1,18 @@
-import express from 'express';
+import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import { sensorRouter } from './routes/sensorRoutes';
 import mongoose from 'mongoose';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 const URL: string = process.env.DB_URL || "";
-mongoose.connect(URL, { dbName: "sensor-database" });
 
-app.use(express.json());
-
-app.use("/sensor", sensorRouter);
-
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+export default function createServer(): Application {
+    const app: Application = express();
+    mongoose.connect(URL, { dbName: "sensor-database" });
+    app.use(express.json());
+    app.use("/sensor", sensorRouter);
+    return app;
+}
