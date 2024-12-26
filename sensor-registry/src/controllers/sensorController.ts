c6e8f56b2839616ba74a76f44310a7606fc29a92
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-26 16:14:28 +0100

    feat: create the handler for shutting off a sensor

--- sensor-registry/src/controllers/sensorController.ts
+++ sensor-registry/src/controllers/sensorController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import HttpStatus from "http-status-codes";
 import { isIpValid } from "./utils/ipUtils";
-import { exists, findAllSensors, saveSensor } from "./utils/sensorUtils";
+import { deleteSensor, exists, findAllSensors, saveSensor } from "./utils/sensorUtils";
 import dotenv from 'dotenv';
 import { fromBody } from "./utils/requestUtils";
 
@@ -13,22 +13,29 @@ const API_KEY_HEADER = String(process.env.API_KEY_HEADER);
 const API_KEY = String(process.env.API_KEY);
 const MAX_PORT = 65_535;
 
-
+function isAuthorized(key: string): boolean {
+    return key !== "" && key === API_KEY;
+}
 
 const registerSensor = async (request: Request, response: Response) => {
     const modelData = request.body;
     if (modelData) {
-        const ip = fromBody(modelData, SENSOR_IP_HEADER, "");
-        const port = fromBody(modelData, SENSOR_PORT_HEADER, -1);
-        if ((port >= 0 && port <= MAX_PORT) && (ip != "" && isIpValid(ip))) {
-            if (!(await exists(ip, port))) {
-                await saveSensor(ip, port);
-                response.status(HttpStatus.CREATED);
-            }else {
+        const apikey = fromBody(modelData, API_KEY_HEADER, "");
+        if (isAuthorized(apikey)) {
+            const ip = fromBody(modelData, SENSOR_IP_HEADER, "");
+            const port = fromBody(modelData, SENSOR_PORT_HEADER, -1);
+            if ((port >= 0 && port <= MAX_PORT) && (ip != "" && isIpValid(ip))) {
+                if (!(await exists(ip, port))) {
+                    await saveSensor(ip, port);
+                    response.status(HttpStatus.CREATED);
+                }else {
+                    response.status(HttpStatus.NOT_ACCEPTABLE);
+                }
+            }else{ 
                 response.status(HttpStatus.NOT_ACCEPTABLE);
             }
-        }else{ 
-            response.status(HttpStatus.NOT_ACCEPTABLE);
+        }else {
+            response.status(HttpStatus.UNAUTHORIZED);
         }
     }else {
         response.status(HttpStatus.BAD_REQUEST);
@@ -39,7 +46,7 @@ const registerSensor = async (request: Request, response: Response) => {
 const allSensors = async (request: Request, response: Response) => {
     const modelData = request.body;
         const apikey = fromBody(modelData, API_KEY_HEADER, "");
-        if (apikey !== "" && apikey === API_KEY){
+        if (isAuthorized(apikey)){
             response.send({sensors: await findAllSensors()});
         }
         else{
@@ -48,4 +55,25 @@ const allSensors = async (request: Request, response: Response) => {
     response.end();
 };
 
-export { registerSensor, allSensors }
+const shutOff = async (request: Request, response: Response) => {
+    const modelData = request.body;
+    if (modelData) {
+        const apikey = fromBody(modelData, API_KEY_HEADER, "");
+        if(isAuthorized(apikey)) {
+            const ip = fromBody(modelData, SENSOR_IP_HEADER, "");
+            const port = fromBody(modelData, SENSOR_PORT_HEADER, -1);
+            if (await exists(ip, port) && await deleteSensor(ip, port)){
+                response.status(HttpStatus.OK);
+            }else{
+                response.status(HttpStatus.NOT_FOUND);
+            }
+        }else{
+            response.status(HttpStatus.UNAUTHORIZED);
+        }
+    }else{
+        response.status(HttpStatus.BAD_REQUEST);
+    }
+    response.end();
+};
+
+export { registerSensor, allSensors, shutOff }
