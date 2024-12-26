6c797b21fb44c736abe30c2fcdb907de2dd88c0a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-26 15:59:10 +0100

    feat: use the sensor router

--- sensor-registry/src/index.ts
+++ sensor-registry/src/index.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { sensorRouter } from './routes/sensorRoutes';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const URL: string = process.env.DB_URL || "";
+mongoose.connect(URL, { dbName: "sensor-database" });
+
 app.use(express.json());
 
-app.get('/', (_, res) => {
-    res.send(`Running in ${process.env.NODE_ENV} mode`);
-});
+app.use("/sensor", sensorRouter);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
