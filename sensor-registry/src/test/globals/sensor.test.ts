453b984d4dd347548d0606d32b450d1c944b3b82
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-26 17:04:27 +0100

    test: add test for registering a sensor without API key or using a wrong API key

--- sensor-registry/src/test/globals/sensor.test.ts
+++ sensor-registry/src/test/globals/sensor.test.ts
@@ -15,18 +15,21 @@ const API_KEY_HEADER = String(process.env.API_KEY_HEADER);
 const API_KEY = String(process.env.API_KEY);
 const MAX_PORT = 65_535;
 
+const sensorIp = "0.0.0.0";
+const sensorPort = 1926;
+
 const sensorInfomration = {
-    [SENSOR_IP_HEADER]: "0.0.0.0",
-    [SENSOR_PORT_HEADER]: 1926,
+    [SENSOR_IP_HEADER]: sensorIp,
+    [SENSOR_PORT_HEADER]: sensorPort,
     [API_KEY_HEADER]: API_KEY
 }
 
 const app = createServer();
 
 describe("Registering a new Sensor", () => {
-    before(() => {
+    before(async () => {
         // Delete
-        shutOffSensor(app, sensorInfomration);
+        await shutOffSensor(app, sensorInfomration);
     })
     it("Registering a new Sensor that does not exists inside the database should be OK", async () =>{
         await request(app)
@@ -34,4 +37,27 @@ describe("Registering a new Sensor", () => {
             .send(sensorInfomration)
             .expect(HttpStatus.CREATED);
     });
+    it("Registering a new Sensor without specifying an API KEY or by using a wrong API Key should return an error", async () => {
+        const noAPI = {
+            [SENSOR_IP_HEADER]: sensorIp,
+            [SENSOR_PORT_HEADER]: sensorPort
+        };
+        const wrongAPI = {
+            [SENSOR_IP_HEADER]: sensorIp,
+            [SENSOR_PORT_HEADER]: sensorPort,
+            [API_KEY_HEADER]: ""
+        }
+        await request(app)
+            .post(REGISTER_SENSOR_PATH)
+            .send(noAPI)
+            .expect(HttpStatus.UNAUTHORIZED);
+        await request(app)
+            .post(REGISTER_SENSOR_PATH)
+            .send(wrongAPI)
+            .expect(HttpStatus.UNAUTHORIZED);
+
+    });
+    afterEach(async () => {
+        await shutOffSensor(app, sensorInfomration);
+    })
 });
