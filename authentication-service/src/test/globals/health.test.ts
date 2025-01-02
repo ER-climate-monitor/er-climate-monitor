c0394d19bb8041767fb3d7f24ccb69fafa7c817c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-02 12:25:47 +0100

    test: add health api status check

--- authentication-service/src/test/globals/health.test.ts
+++ authentication-service/src/test/globals/health.test.ts
@@ -3,4 +3,14 @@ import createServer from "../..";
 import { describe, it } from "mocha";
 import HttpStatus from "http-status-codes";
 import { Application } from "express";
-describe("Health status of the server", () => {});
+
+const HEALTH_API_ROUTE = "/health/status";
+const app: Application = createServer();
+
+describe("Health status of the server", () => {
+    it("Pinging the Health API should return ok.", async () => {
+        await request(app)
+            .get(HEALTH_API_ROUTE)
+            .expect(HttpStatus.OK);
+    });
+});
