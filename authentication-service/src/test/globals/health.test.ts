8fe63819960bcc0d29c0f0ae3a450182a988f00f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-02 12:23:17 +0100

    test: init health api test

---
+++ authentication-service/src/test/globals/health.test.ts
@@ -0,0 +1,6 @@
+import request from "supertest"
+import createServer from "../..";
+import { describe, it } from "mocha";
+import HttpStatus from "http-status-codes";
+import { Application } from "express";
+describe("Health status of the server", () => {});
