5c176589c05cc1469c275a24b06eb9f7dbf3aa9e
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-06 14:57:45 +0100

    feat: add decodeToken and tokenExpiration functions

--- authentication-service/src/controllers/utils/jwt.ts
+++ authentication-service/src/controllers/utils/jwt.ts
@@ -7,12 +7,20 @@ import { Token } from "../../models/tokenModel";
 
 dotenv.config();
 
+function decodeToken(token: string): JwtPayload { 
+    return jwtDecode(token);
+}
+
+function tokenExpiration(token: string): Date {
+    const infos = decodeToken(token);
+    return new Date((infos.exp || 0) * 1000);
+}
+
 async function createToken(inputEmail: string): Promise<Token> {
     const EXPIRATION = process.env.EXPIRATION || "1h";
     const data = { userEmail:inputEmail,};
     const token = jwt.sign(data, jwtSecretKey, {expiresIn: EXPIRATION});
-    const infos: JwtPayload= jwtDecode<JwtPayload>(token);
-    return new Token(token, new Date((infos.exp || 0) * 1000));
+    return new Token(token, new Date(tokenExpiration(token)));
 }
 
 function verifyToken(token: string): Boolean {
@@ -31,4 +39,4 @@ function verifyToken(token: string): Boolean {
     }
 };
 
-export { createToken, verifyToken }
+export { createToken, verifyToken, tokenExpiration }
