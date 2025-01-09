e99a7de8c9c7cd1b55ee79cd41080ecc2c09ba8f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-09 18:39:12 +0100

    refactor: use const variable inside the router

--- authentication-service/src/routes/v0/healthRouter.ts
+++ authentication-service/src/routes/v0/healthRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { health } from "../../controllers/v0/healthController";
+import { STATUS_PATH } from "./paths/health.paths";
 
 const healthRouter = express.Router();
 
 
 
-healthRouter.route("/status")
+healthRouter.route(STATUS_PATH)
     .get(health);
 
 export { healthRouter }
