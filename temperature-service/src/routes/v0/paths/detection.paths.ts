3247a95736d4b61a81fef408c3550f83932dea72
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-10 14:48:26 +0100

    feat: add new query variable for query a specific timespan of detectins

--- temperature-service/src/routes/v0/paths/detection.paths.ts
+++ temperature-service/src/routes/v0/paths/detection.paths.ts
@@ -1,4 +1,11 @@
-const sensorIdParameter: string = "sensorId"
+const sensorIdParameter: string = "sensorId";
+const LAST_DETECTION_QUERY_VARIABLE = "last";
+const FROM_TIMESTAMP_QUERY_VALUE = "from";
+const TO_TIMESTAMP_QUERY_VALUE = "to";
 const SAVE_DETECTION_PATH = "/detection";
 const DETECTIONS_FROM_SENSOR_PATH = "/{" + sensorIdParameter + "}/detections";
-export { sensorIdParameter, SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH }
+
+
+export { sensorIdParameter, LAST_DETECTION_QUERY_VARIABLE, 
+    FROM_TIMESTAMP_QUERY_VALUE, TO_TIMESTAMP_QUERY_VALUE, 
+    SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH }
