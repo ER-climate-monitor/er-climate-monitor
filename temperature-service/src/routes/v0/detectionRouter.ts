37f55dd6423c07887188a4bc3e6d53c17a6d5320
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-10 14:37:48 +0100

    feat: add route for querying detections

--- temperature-service/src/routes/v0/detectionRouter.ts
+++ temperature-service/src/routes/v0/detectionRouter.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import { SAVE_DETECTION_PATH } from "./paths/detection.paths";
-import { saveDetection } from "../../controllers/v0/detectionController";
+import { DETECTIONS_FROM_SENSOR_PATH, SAVE_DETECTION_PATH } from "./paths/detection.paths";
+import { saveDetection, getDetectionsFromSensor } from "../../controllers/v0/detectionController";
+import { Detection } from "../../models/v0/detectionModel";
 
 const detectionRouter = express.Router();
 
 detectionRouter.route(SAVE_DETECTION_PATH)
     .post(saveDetection);
+
+detectionRouter.route(DETECTIONS_FROM_SENSOR_PATH)
+    .get(getDetectionsFromSensor);
 
 export { detectionRouter }
