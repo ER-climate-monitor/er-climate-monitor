18b72a38e895ba5a4488af1d5352ecf5ca58dcd9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-10 14:43:28 +0100

    refactor: move the parameter value inside a new variable

--- temperature-service/src/routes/v0/paths/detection.paths.ts
+++ temperature-service/src/routes/v0/paths/detection.paths.ts
@@ -1,3 +1,4 @@
+const sensorIdParameter: string = "sensorId"
 const SAVE_DETECTION_PATH = "/detection";
-const DETECTIONS_FROM_SENSOR_PATH = "/{sensorId}/detections";
-export { SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH }
+const DETECTIONS_FROM_SENSOR_PATH = "/{" + sensorIdParameter + "}/detections";
+export { sensorIdParameter, SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH }
