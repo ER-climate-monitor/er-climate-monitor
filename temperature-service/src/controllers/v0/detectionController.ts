0aebaf1f96c13fdd13501f71f172eef53aea40ff
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-10 12:42:11 +0100

    feat: init saveDetection handler

--- temperature-service/src/controllers/v0/detectionController.ts
+++ temperature-service/src/controllers/v0/detectionController.ts
@@ -2,3 +2,11 @@ import { Request, Response } from "express";
 import HttpStatus from "http-status-codes";
 import dotenv from "dotenv";
 
+const saveDetection = async (request: Request, response: Response) {
+    const modelData = request.body;
+    if (modelData) {
+        
+    }
+    response.end();
+
+}
