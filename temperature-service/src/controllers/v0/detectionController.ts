cbe9a5847af4f83365e4d1ce90859793cea0e0ad
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-10 14:21:38 +0100

    feat: create the handler for saving incoming detections from a sensor

--- temperature-service/src/controllers/v0/detectionController.ts
+++ temperature-service/src/controllers/v0/detectionController.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from "express";
 import HttpStatus from "http-status-codes";
 import dotenv from "dotenv";
+import { saveDetectionModel } from "./utils/detectionUtils";
+
+function fromBody<X>(body: any, key: string, defaultValue: X) {
+    return body && key in body ? body[key] : defaultValue;
+}
+
 
 const saveDetection = async (request: Request, response: Response) {
     const modelData = request.body;
     if (modelData) {
-        
+        try{
+            const newDetection = await saveDetectionModel(
+                fromBody(modelData, String(process.env.SENSOR_ID_HEADER), ""),
+                fromBody(modelData, String(process.env.SENSOR_NAME_HEADER), ""),
+                fromBody(modelData, String(process.env.SENSOR_DETECTION_UNIT_HEADER), ""),
+                fromBody(modelData, String(process.env.SENSOR_DETECTION_TIMESTAMP_HEADER), 0),
+                fromBody(modelData, String(process.env.SENSOR_DETECTION_LONGITUDE_HEADER), 0),
+                fromBody(modelData, String(process.env.SENSOR_DETECTION_LATITUDE_HEADER), 0),
+                fromBody(modelData, String(process.env.SENSOR_DETECTION_VALUE), 0));
+            response.status(HttpStatus.CREATED);
+        }catch(error) {
+            response.status(HttpStatus.BAD_REQUEST)
+                .send({
+                    [String(process.env.ERROR_TAG)]: error
+                });
+        }
     }
     response.end();
 
