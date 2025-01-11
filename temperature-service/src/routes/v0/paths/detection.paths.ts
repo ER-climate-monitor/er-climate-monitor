866efb2d5b3fd423dcd9fad6032c91dcc082e558
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-11 12:05:32 +0100

    fix: set correct url parameter syntax

--- temperature-service/src/routes/v0/paths/detection.paths.ts
+++ temperature-service/src/routes/v0/paths/detection.paths.ts
@@ -3,7 +3,7 @@ const LAST_DETECTION_QUERY_VARIABLE = "last";
 const FROM_TIMESTAMP_QUERY_VALUE = "from";
 const TO_TIMESTAMP_QUERY_VALUE = "to";
 const SAVE_DETECTION_PATH = "/detection";
-const DETECTIONS_FROM_SENSOR_PATH = "/{" + sensorIdParameter + "}/detections";
+const DETECTIONS_FROM_SENSOR_PATH = "/:" + sensorIdParameter + "/detections";
 
 
 export { sensorIdParameter, LAST_DETECTION_QUERY_VARIABLE, 
