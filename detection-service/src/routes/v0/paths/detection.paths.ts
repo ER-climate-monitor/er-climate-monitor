7e7f09cc40c154c67e1ec49ae4b29a8a6b6ded15
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-12 12:06:33 +0100

    feat: add const variables for generic path, temperature path service

--- detection-service/src/routes/v0/paths/detection.paths.ts
+++ detection-service/src/routes/v0/paths/detection.paths.ts
@@ -1,11 +1,16 @@
+const GENERIC_SENSOR_PATH = "/v0/sensor";
+const TEMPERATURE_PATH_V0 = GENERIC_SENSOR_PATH + "/temperature";
+
+
 const sensorIdParameter: string = "sensorId";
 const LAST_DETECTION_QUERY_VARIABLE = "last";
 const FROM_TIMESTAMP_QUERY_VALUE = "from";
 const TO_TIMESTAMP_QUERY_VALUE = "to";
 const SAVE_DETECTION_PATH = "/detection";
-const DETECTIONS_FROM_SENSOR_PATH = "/:" + sensorIdParameter + "/detections";
+const DETECTIONS_FROM_SENSOR_PATH = `/:${sensorIdParameter}/detections`;
 
 
 export { sensorIdParameter, LAST_DETECTION_QUERY_VARIABLE, 
     FROM_TIMESTAMP_QUERY_VALUE, TO_TIMESTAMP_QUERY_VALUE, 
-    SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH }
+    SAVE_DETECTION_PATH, DETECTIONS_FROM_SENSOR_PATH,
+    TEMPERATURE_PATH_V0 }
