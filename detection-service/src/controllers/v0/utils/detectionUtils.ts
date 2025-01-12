1aa8d7a252d07dd944c41960589855bf0e1f9305
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-12 12:04:47 +0100

    feat: add model document variable inside functions signatures

--- detection-service/src/controllers/v0/utils/detectionUtils.ts
+++ detection-service/src/controllers/v0/utils/detectionUtils.ts
@@ -1,16 +1,27 @@
+import { Model } from "mongoose";
 import { Detection } from "../../../models/v0/detectionModel";
-import { detectionModel, DetectionDocument } from "../../../models/v0/detectionModel";
+import { temperatureDetections, DetectionDocument } from "../../../models/v0/detectionModel";
+import { TEMPERATURE } from "../../../models/v0/detectionTypes";
 
 const LOWER_BOUND = 0;
 const UPPER_BOUND = 100;
+
+function getModelFromDetectionType(type: String): Model<DetectionDocument> {
+    switch (type) {
+        case (TEMPERATURE): {
+            return temperatureDetections;
+        }
+    }
+    throw new Error("Illegal Argument Exception, the input parameter type is wrong");
+}
 
 function createDetection(sensorId: string, sensorName: string, unit: string, timestamp: number, longitude: number, latitude: number, value: number): Detection {
     return new Detection(sensorId, sensorName, unit, timestamp, longitude, latitude, value);
 }
 
-async function saveDetectionModel(sensorId: string, sensorName: string, unit: string, timestamp: number, longitude: number, latitude: number, value: number): Promise<DetectionDocument> {
-    const detection: Detection = createDetection(sensorId, sensorName, unit, timestamp, longitude, latitude, value);
-    const newDetection: DetectionDocument = new detectionModel({
+
+async function saveDetection(model: Model<DetectionDocument>, detection: Detection): Promise<DetectionDocument> {
+    const newDetection: DetectionDocument = new model({
         sensorId: detection.sensorId,
         sensorName: detection.sensorName,
         unit: detection.unit,
@@ -23,16 +34,21 @@ async function saveDetectionModel(sensorId: string, sensorName: string, unit: st
     return newDetection
 }
 
-async function checkSensorID(sensorId: string): Promise<Boolean> {
-    const exists = await detectionModel.exists({sensorId: sensorId});
+async function saveDetectionModel(model: Model<DetectionDocument>, sensorId: string, sensorName: string, unit: string, timestamp: number, longitude: number, latitude: number, value: number): Promise<DetectionDocument> {
+    const detection: Detection = createDetection(sensorId, sensorName, unit, timestamp, longitude, latitude, value);
+    return saveDetection(model, detection);
+}
+
+async function checkSensorID(model: Model<DetectionDocument>, sensorId: string): Promise<Boolean> {
+    const exists = await model.exists({sensorId: sensorId});
     return exists !== null;
 }
 
-async function getLastXDetections(sensorId: string, last: number): Promise<Array<Detection>> {
+async function getLastXDetections(model: Model<DetectionDocument>, sensorId: string, last: number): Promise<Array<Detection>> {
     if (last <= LOWER_BOUND || last > UPPER_BOUND) {
         throw new Error(`The query prameter last, must be in the interval: [${LOWER_BOUND}, ${UPPER_BOUND}]`);
     }
-    return (await detectionModel.find({sensorId: sensorId})
+    return (await model.find({sensorId: sensorId})
         .sort({_id: -1})
         .limit(last))
         .map(detection => { 
