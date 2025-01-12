38b0bb1bdfbd6d4816aa906726f8cade74a2751d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2025-01-12 12:05:16 +0100

    refactor: change file name and use generic input functions

--- detection-service/src/controllers/v0/temperatureController.ts
+++ detection-service/src/controllers/v0/temperatureController.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import HttpStatus from "http-status-codes";
 import { checkSensorID, getLastXDetections, saveDetectionModel } from "./utils/detectionUtils";
 import { FROM_TIMESTAMP_QUERY_VALUE, LAST_DETECTION_QUERY_VARIABLE, sensorIdParameter, TO_TIMESTAMP_QUERY_VALUE } from "../../routes/v0/paths/detection.paths";
+import { Mode } from "fs";
+import { DetectionDocument, temperatureDetections } from "../../models/v0/detectionModel";
+import { handleGetDetectionsFromSensor, handleSaveDetection } from "./utils/handlers";
+import { model } from "mongoose";
 
 
 function fromBody<X>(body: any, key: string, defaultValue: X) {
@@ -13,14 +17,8 @@ const saveDetection = async (request: Request, response: Response) => {
     const modelData = request.body;
     if (modelData) {
         try{
-            const newDetection = await saveDetectionModel(
-                fromBody(modelData, String(process.env.SENSOR_ID_HEADER), ""),
-                fromBody(modelData, String(process.env.SENSOR_NAME_HEADER), ""),
-                fromBody(modelData, String(process.env.SENSOR_DETECTION_UNIT_HEADER), ""),
-                fromBody(modelData, String(process.env.SENSOR_DETECTION_TIMESTAMP_HEADER), 0),
-                fromBody(modelData, String(process.env.SENSOR_DETECTION_LONGITUDE_HEADER), 0),
-                fromBody(modelData, String(process.env.SENSOR_DETECTION_LATITUDE_HEADER), 0),
-                fromBody(modelData, String(process.env.SENSOR_DETECTION_VALUE_HEADER), 0));
+            console.log(modelData);
+            const newDetection = await handleSaveDetection(temperatureDetections, modelData);
             response.status(HttpStatus.CREATED);
         }catch(error) {
             response.status(HttpStatus.BAD_REQUEST)
@@ -36,21 +34,11 @@ const getDetectionsFromSensor = async (request: Request, response: Response) => 
     if (sensorIdParameter in request.params) {
         const sensorId: string = request.params.sensorId;
         try {
-            if (await checkSensorID(sensorId)) {
-                if (LAST_DETECTION_QUERY_VARIABLE in request.query){
-                    const days = Number(request.query[LAST_DETECTION_QUERY_VARIABLE]);
-                    response.status(HttpStatus.OK)
-                        .send({
-                            [String(process.env.SENSOR_DETECTIONS_HEADER)]: await getLastXDetections(sensorId, days)
+            if (await checkSensorID(temperatureDetections, sensorId)) {
+                response.status(HttpStatus.OK)
+                    .send({
+                        [String(process.env.SENSOR_DETECTIONS_HEADER)]: await handleGetDetectionsFromSensor(temperatureDetections, sensorId, request)
                     });
-                }else if(FROM_TIMESTAMP_QUERY_VALUE in request.query && TO_TIMESTAMP_QUERY_VALUE in request.query){
-                    
-                }else{
-                    response.status(HttpStatus.NOT_ACCEPTABLE)
-                        .send({
-                            [String(process.env.ERROR_TAG)]: "Missing the query variable"
-                        });
-                }
             }else{
                 response.status(HttpStatus.NOT_FOUND)
                     .send({
