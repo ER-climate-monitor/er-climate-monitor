7ac33eb77f795f2eb54dc099ed14fe99b2f8a252
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-13 12:11:58 +0100

    refactor: move all the modules inside the v0 folder

---
+++ sensor-registry/src/model/v0/headers/sensorHeaders.ts
@@ -0,0 +1,5 @@
+const SENSOR_PORT_HEADER = "X-Sensor-Port";
+const SENSOR_IP_HEADER = "X-Sensor-Ip";
+const API_KEY_HEADER = "X-Api-Key";
+
+export { SENSOR_IP_HEADER, SENSOR_PORT_HEADER, API_KEY_HEADER }
