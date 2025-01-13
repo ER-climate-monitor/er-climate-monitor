045870feeee7b9fdd91f27c50fdf643dcbdc8026
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-13 12:16:01 +0100

    refactor: create const variables for all the different paths, use the const variables inside the sensor router

---
+++ sensor-registry/src/routes/v0/paths/sensorPaths.ts
@@ -0,0 +1,7 @@
+const BASE_SENSOR_PATH_V0 = "/v0/sensor"
+
+const REGISTER_PATH = "/register";
+const ALL_PATH = "/all";
+const SHUT_OFF_PATH = "/shutoff";
+
+export { BASE_SENSOR_PATH_V0, REGISTER_PATH, ALL_PATH, SHUT_OFF_PATH }
--- sensor-registry/src/routes/v0/sensorRoutes.ts
+++ sensor-registry/src/routes/v0/sensorRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express"
 import { allSensors, registerSensor, shutOff } from "../../controllers/v0/sensorController";
+import { ALL_PATH, REGISTER_PATH, SHUT_OFF_PATH } from "./paths/sensorPaths";
 
 const sensorRouter = express.Router();
 
-sensorRouter.route("/register")
+sensorRouter.route(REGISTER_PATH)
     .post(registerSensor);
 
-sensorRouter.route("/all")
+sensorRouter.route(ALL_PATH)
     .get(allSensors)
 
-sensorRouter.route("/shutoff")
+sensorRouter.route(SHUT_OFF_PATH)
     .delete(shutOff);
 
 export { sensorRouter }
