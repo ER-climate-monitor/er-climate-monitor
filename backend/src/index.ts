afbb253e6f4d4afda922030e01c355551a2f4707
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-14 11:30:56 +0100

    feat: use gateway router for the base path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { BASE_PATH_V0 } from './routes/v0/paths/gatewayPaths';
+import { gatewayRouter } from './routes/v0/gatewayRouter';
 
 dotenv.config();
 
@@ -8,8 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (_, res) => {
+app.get(BASE_PATH_V0, (_, res) => {
     res.send(`Running in ${process.env.NODE_ENV} mode`);
 });
+
+app.use(BASE_PATH_V0, gatewayRouter)
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
