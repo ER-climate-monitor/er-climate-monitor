09a833c285859a2fc076c3b2c73a51f063465f1e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-14 12:25:50 +0100

    feat: add post handler

--- backend/src/routes/v0/gatewayRouter.ts
+++ backend/src/routes/v0/gatewayRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { AUTHENTICATION_PATHS } from "./paths/gatewayPaths";
-import { authenticationGetHandler } from "../../controllers/v0/gatewayController";
+import { authentiationPostHandler, authenticationGetHandler } from "../../controllers/v0/gatewayController";
 
 const gatewayRouter = express.Router();
 
 gatewayRouter.route(AUTHENTICATION_PATHS)
     .get(authenticationGetHandler)
+    .post(authentiationPostHandler)
 
 export { gatewayRouter }
