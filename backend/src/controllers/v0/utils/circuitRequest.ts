f656b86c0ba44fbd666756bf652655b36b8190d6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-14 11:14:50 +0100

    feat: add circuit breaker default options

--- backend/src/controllers/v0/utils/circuitRequest.ts
+++ backend/src/controllers/v0/utils/circuitRequest.ts
@@ -1,9 +1,20 @@
 import CircuitBreaker from "opossum";
+import axios from "axios"
 
-async function makeRequest(service: string, path: string, data: any) {
+const options = {
+    timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+    errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+    resetTimeout: 30000 // After 30 seconds, try again.
+};
+
+async function makeRequest(service: string, operation: string,  path: string, data: any) {
+    
+}
+
+async function getRequest(service: string, path: string, data: any) {
 
 }
 
-const breaker = new CircuitBreaker(makeRequest);
+const breaker = new CircuitBreaker(makeRequest, options);
 
 export { breaker }
