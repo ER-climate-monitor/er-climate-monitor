0f7eaaf2e10a2044754bfee69b786594286e37df
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-14 12:25:44 +0100

    feat: use promises

--- backend/src/controllers/v0/gatewayController.ts
+++ backend/src/controllers/v0/gatewayController.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from "express"
 import { breaker } from "./utils/circuitRequest"
 import { AUTHENTICATION_ENDPOINT } from "../../models/v0/serviceModels"
-import { GET } from "./utils/httpMethods"
+import { GET, POST } from "./utils/httpMethods"
 import { removeServiceFromUrl } from "./utils/urlUtils"
 import { AUTHENTICATION_SERVICE } from "../../routes/v0/paths/gatewayPaths"
+import { HttpStatusCode } from "axios"
 
 
 const authenticationGetHandler = async (request: Request, response: Response) => {
-    const url = request.url
-    const endpointPath = removeServiceFromUrl(AUTHENTICATION_SERVICE, url);
-    breaker.fire(AUTHENTICATION_ENDPOINT, GET, endpointPath, request.headers, request.body);
+    try {
+        const url = request.url
+        const endpointPath = removeServiceFromUrl(AUTHENTICATION_SERVICE, url);
+        breaker.fire(AUTHENTICATION_ENDPOINT, GET, endpointPath, request.headers, request.body);
+    }catch(error) {
+        console.log(error);
+    }
     response.end();
 }
 
-export { authenticationGetHandler }
+const authentiationPostHandler = async (request: Request, response: Response) => {
+    const url = request.url
+    const endpointPath = removeServiceFromUrl(AUTHENTICATION_SERVICE, url);
+    breaker.fire(AUTHENTICATION_ENDPOINT, POST, endpointPath, request.headers, request.body)
+        .then()
+        .catch(error => {
+            response.status(HttpStatusCode.BadRequest);
+            response.end();
+        });
+}
+
+export { authenticationGetHandler, authentiationPostHandler }
