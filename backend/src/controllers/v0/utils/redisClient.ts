e0c072bd594aaf4cceaa2d7abb0c0889b121c963
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-15 11:43:08 +0100

    feat: create Authentication Client class

--- backend/src/controllers/v0/utils/redisClient.ts
+++ backend/src/controllers/v0/utils/redisClient.ts
@@ -4,6 +4,17 @@ const options = {
     url: process.env.REDIS_URL
 }
 
-const authenticationRedisClient = createClient(options);
+class AuthenticationClient {
+    authenticationRedisClient: any
+    constructor(options: {}) {
+        this.authenticationRedisClient = createClient(options);
+    }
+
+    public async setToken(token: string, expiration: number | string) {
+        this.authenticationRedisClient.set
+    }
+}
+
+const authenticationRedisClient = new AuthenticationClient(options);
 
 export { authenticationRedisClient }
