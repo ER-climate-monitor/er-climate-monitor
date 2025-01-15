cb63ec57f09430e913c8641e1be178d39712a08a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-15 11:51:36 +0100

    feat: extract from response token and Its expiration

--- backend/src/controllers/v0/authenticationController.ts
+++ backend/src/controllers/v0/authenticationController.ts
@@ -5,7 +5,9 @@ import { GET, POST } from "./utils/httpMethods"
 import { removeServiceFromUrl } from "./utils/urlUtils"
 import { AUTHENTICATION_SERVICE } from "../../routes/v0/paths/gatewayPaths"
 import { HttpStatusCode } from "axios"
-
+import { authenticationRedisClient } from "./utils/redisClient"
+import { fromAxiosToResponse } from "./utils/responseUtils"
+import { USER_JWT_TOKEN_EXPIRATION_HEADER, USER_JWT_TOKEN_HEADER } from "../../models/v0/headers/authenticationHeaders"
 
 const authenticationGetHandler = async (request: Request, response: Response) => {
     try {
@@ -22,7 +24,12 @@ const authentiationPostHandler = async (request: Request, response: Response) =>
     const url = request.url
     const endpointPath = removeServiceFromUrl(AUTHENTICATION_SERVICE, url);
     breaker.fire(AUTHENTICATION_ENDPOINT, POST, endpointPath, request.headers, request.body)
-        .then()
+        .then(axiosResponse => {
+            response = fromAxiosToResponse(axiosResponse, response);
+            if (response.statusCode === HttpStatusCode.Ok || response.statusCode === HttpStatusCode.Created) {
+                authenticationRedisClient.setToken(String(response.getHeader(USER_JWT_TOKEN_HEADER)), String(response.getHeader(USER_JWT_TOKEN_EXPIRATION_HEADER)));
+            }
+        })
         .catch(error => {
             response.status(HttpStatusCode.BadRequest);
             response.end();
