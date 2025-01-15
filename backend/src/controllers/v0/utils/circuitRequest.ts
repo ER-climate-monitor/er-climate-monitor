310086887f0dc7b7bd7b50aa793fc4c471774746
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-15 12:39:52 +0100

    refactor: create circuit breaker class. Update circuit breaker options

--- backend/src/controllers/v0/utils/circuitRequest.ts
+++ backend/src/controllers/v0/utils/circuitRequest.ts
@@ -2,53 +2,71 @@ import CircuitBreaker from "opossum";
 import axios, { AxiosError, AxiosResponse } from "axios"
 import { DELETE, GET, POST, PUT } from "./httpMethods";
 
-const options = {
-    timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+const defaultOptions = {
+    timeout: 6000, // If our function takes longer than 3 seconds, trigger a failure
     errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
     resetTimeout: 30000 // After 30 seconds, try again.
 };
 
-const breaker = new CircuitBreaker(makeRequest, options);
+class CircuitBreakerClient { 
+    private breaker: CircuitBreaker;
+    constructor (options: {}) {
+        this.breaker = new CircuitBreaker(this.makeRequest.bind(this), options);
+    }
 
-async function makeRequest(service: string, method: string,  path: string, headers: any, body: any): Promise<AxiosResponse<any, any>> {
-    const endpoint = service + path;
-    console.log(endpoint);
-    try {
-        switch (method) {
-            case (GET): {
-                return getRequest(endpoint, headers);
+    async fireRequest(service: string, method: string,  path: string, headers: any, body: any): Promise<AxiosResponse<any, any>> {
+        return this.breaker.fire(service, method, path, headers, body) as Promise<AxiosResponse<any, any>>;
+    }
+
+    private async makeRequest(service: string, method: string,  path: string, headers: any, body: any): Promise<AxiosResponse<any, any>> {
+        const endpoint = service + path;
+        try {
+            switch (method) {
+                case (GET): {
+                    return this.getRequest(endpoint, headers);
+                }
+                case (POST): {
+                    return this.postRequest(endpoint, headers, body);
+                }
+                case (PUT): {
+                    // TODO
+                }
+                case (DELETE): {
+                    // TODO
+                }
             }
-            case (POST): {
-                return postRequest(endpoint, headers, body);
-            }
-            case (PUT): {
-                // TODO
-            }
-            case (DELETE): {
-                // TODO
-            }
+        }catch(error) {
+            this.breaker.fallback(() => "Error, the service is out. Try again later.");
         }
-    }catch(error) {
-        breaker.fallback(() => "Error, the service is out. Try again later.");
+        throw new Error("Error, connection refused");
     }
-    throw new Error("Error, connection refused");
-}
 
-async function getRequest(endpoint: string, headers: any): Promise<AxiosResponse<any, any>> {
-    try {
-        return axios.get(endpoint, headers);
-    }catch(error) {
-        throw error;
+    private async getRequest(endpoint: string, headers: any): Promise<AxiosResponse<any, any>> {
+        try {
+            return axios.get(endpoint, headers);
+        }catch(error) {
+            throw error;
+        }
     }
-}
 
-async function postRequest(endpoint: string, headers: any, body: any): Promise<AxiosResponse<any, any>>{
-    try {
-        return axios.post(endpoint, body, headers);
-    }catch(error) {
-        throw new Error("Error, connection refused from: " + endpoint);
+    private async postRequest(endpoint: string, headers: any, body: any): Promise<AxiosResponse<any, any>>{
+        try {
+            return axios.post(endpoint, body, headers);
+        }catch(error) {
+            throw new Error("Error, connection refused from: " + endpoint);
+        }
     }
 }
 
+class BreakerFactory {
+    static breakerWithOptions(options: {}) {
+        return new CircuitBreakerClient(options)
+    }
+    
+    static breakerWithDefaultOptions() {
+        return new CircuitBreakerClient(defaultOptions);
+    }
+}
 
-export { breaker }
+
+export { BreakerFactory, defaultOptions }
