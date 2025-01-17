296df0d454bd1585d36a594b796c47d85f2385b9
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 12:11:35 +0100

    refactor: refactor user body fields

---
+++ backend/src/models/v0/authentication/headers/authenticationHeaders.ts
@@ -0,0 +1,19 @@
+const USER_EMAIL_BODY = 'userEmail';
+const USER_JWT_TOKEN_BODY = 'userToken';
+const USER_PASSWORD_BODY = 'userPassword';
+const USER_JWT_TOKEN_EXPIRATION_BODY = 'userTokenExpiration';
+const USER_ACTION_BODY = 'action';
+
+const REGISTER_ACTION = "register";
+const LOGIN_ACTION = "login";
+const AUTHENTICATE_ACTION = "authenticate";
+
+export { USER_EMAIL_BODY,
+     USER_JWT_TOKEN_BODY,
+      USER_PASSWORD_BODY,
+       USER_JWT_TOKEN_EXPIRATION_BODY,
+        USER_ACTION_BODY,
+        REGISTER_ACTION,
+         LOGIN_ACTION,
+          AUTHENTICATE_ACTION
+ };
