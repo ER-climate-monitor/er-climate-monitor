73b36135d5779247cd3418689c491f3fc0b8aae6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 16:22:31 +0100

    refactor: delete get handler

--- backend/src/routes/v0/gatewayRouter.ts
+++ backend/src/routes/v0/gatewayRouter.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import { AUTHENTICATION_PATHS } from './paths/gatewayPaths';
-import { authenticationGetHandler } from '../../controllers/v0/authenticationController';
 import { authentiationPostHandler } from '../../controllers/v0/authenticationController/authenticationPostController';
 
 const gatewayRouter = express.Router();
 
-gatewayRouter.route(AUTHENTICATION_PATHS).get(authenticationGetHandler).post(authentiationPostHandler);
+gatewayRouter.route(AUTHENTICATION_PATHS).post(authentiationPostHandler);
 
 export { gatewayRouter };
