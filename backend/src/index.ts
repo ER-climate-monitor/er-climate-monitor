5d446558c4067f4b8528fac84edbb611da7a8324
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 17:38:24 +0100

    refactor: use main gateway router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { BASE_PATH_V0 } from './routes/v0/paths/gatewayPaths';
-import { authenticationRouter } from './routes/v0/authentication/authenticationRouter';
-
+import { gatewayRouter } from './routes/v0/gatewayRouter';
 dotenv.config();
 
 const app = express();
@@ -14,6 +13,6 @@ app.get(BASE_PATH_V0, (_, res) => {
     res.send(`Running in ${process.env.NODE_ENV} mode`);
 });
 
-app.use(BASE_PATH_V0, authenticationRouter);
+app.use(BASE_PATH_V0, gatewayRouter);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
