b818f7a3fcb11aaf862665a7991a88bdd1b00be1
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 17:32:55 +0100

    refactor: create authentication router

--- backend/src/routes/v0/gatewayRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { AUTHENTICATION_PATHS } from './paths/gatewayPaths';
-import { authentiationPostHandler } from '../../controllers/v0/domain/authentication/authenticationPostController';
-
-const gatewayRouter = express.Router();
-
-gatewayRouter.route(AUTHENTICATION_PATHS).post(authentiationPostHandler);
-
-export { gatewayRouter };
