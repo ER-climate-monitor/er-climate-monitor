557fead5cfa275b154b132ece2500b0050fea1db
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 15:37:02 +0100

    refactor: use await instead of then inside login handler

--- backend/src/controllers/v0/authenticationController.ts
+++ backend/src/controllers/v0/authenticationController.ts
@@ -80,25 +80,26 @@ const authentiationPostHandler = async (request: Request, response: Response) =>
             return;
         }
         case LOGIN_ACTION: {
-            authenticationService
-                .loginOperation(endpointPath, request.headers, request.body)
-                .then(async (axiosResponse: AxiosResponse<any, any>) => {
-                    response = fromAxiosToResponse(axiosResponse, response);
-                    if (response.statusCode === HttpStatusCode.Ok) {
-                        Logger.info('User correctly logged in. Saving the token and Its expiration');
-                        saveToken(response);
-                    }
-                    response.send(axiosResponse.data);
-                })
-                .catch((error) => {
-                    Logger.error("Error during user's login " + error);
-                    if (error instanceof AxiosError) {
-                        response = handleError(error, response);
-                    }
-                })
-                .finally(() => {
-                    response.end();
-                });
+            try {
+                const axiosResponse = await authenticationService.loginOperation(
+                    endpointPath,
+                    request.headers,
+                    request.body,
+                );
+                response = fromAxiosToResponse(axiosResponse, response);
+                if (response.statusCode === HttpStatusCode.Ok) {
+                    Logger.info('User correctly logged in. Saving the token and Its expiration');
+                    saveToken(response);
+                }
+                response.send(axiosResponse.data);
+            } catch (error) {
+                Logger.error("Error during user's login " + error);
+                if (error instanceof AxiosError) {
+                    response = handleError(error, response);
+                }
+            } finally {
+                response.end();
+            }
             return;
         }
         default: {
