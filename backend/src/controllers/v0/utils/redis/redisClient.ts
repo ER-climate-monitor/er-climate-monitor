16656c0dbbff3a4af8913c9106fdef52d52c1043
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 15:48:00 +0100

    feat: add attribute type, add search token operation

--- backend/src/controllers/v0/utils/redis/redisClient.ts
+++ backend/src/controllers/v0/utils/redis/redisClient.ts
@@ -5,15 +5,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class AuthenticationClient {
-    authenticationRedisClient: any;
+    authenticationRedisClient: Redis;
     constructor() {
         this.authenticationRedisClient = new Redis(String(process.env.REDIS_URL));
     }
 
-    public async setToken(token: string, expiration: string) {
+    public async setToken(token: string, expiration: string): Promise<void> {
         if (this.checkInput(token) && this.checkInput(expiration)) {
             this.authenticationRedisClient.set(token, String(expiration));
         }
+    }
+
+    public async searchToken(token: string): Promise<string | null> {
+        return this.authenticationRedisClient.get(token);
     }
 
     private checkInput(input: string): boolean {
