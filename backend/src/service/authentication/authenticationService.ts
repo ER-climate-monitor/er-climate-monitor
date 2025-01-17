8ca49f3bf1378d7e1b9ada0cb4c6cb5d50dd4c0b
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-17 12:11:09 +0100

    feat: init authentication service

---
+++ backend/src/service/authentication/authenticationService.ts
@@ -0,0 +1,30 @@
+import { POST } from "../../controllers/v0/utils/api/httpMethods";
+import { CircuitBreakerClient } from "../../controllers/v0/utils/circuitBreaker/circuitRequest";
+import { HttpClient } from "../../controllers/v0/utils/circuitBreaker/http/httpClient";
+
+interface AuthenticationOperations<X> {
+    registerOperation(endpointPath: string, headers: any, body: any): Promise<X>
+    loginOperation(endpointPath: string, headers: any, body: any): Promise<X>
+}
+
+
+class AuthenticationService<T extends HttpClient<X>, X> implements AuthenticationOperations<X> {
+    private authenticationEndpoint: string;
+    private circuitBreaker: CircuitBreakerClient<T, X>;
+
+    constructor(circuitBreaker: CircuitBreakerClient<T, X>, endpoint: string) {
+        this.authenticationEndpoint = endpoint;
+        this.circuitBreaker = circuitBreaker;
+    }
+
+    public async registerOperation(endpointPath: string, headers: any, body: any): Promise<X> {
+        return this.circuitBreaker.fireRequest(this.authenticationEndpoint, POST, endpointPath, headers, body);
+    }
+
+    public loginOperation(endpointPath: string, headers: any, body: any): Promise<X> {
+        throw new Error("TODO");
+    }
+    
+}
+
+export { AuthenticationService }
