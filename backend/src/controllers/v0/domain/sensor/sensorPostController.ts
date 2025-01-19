09d30b1f5dc5ab462fba36869952a67697c096f4
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 16:06:21 +0100

    refactor: import sensor service

--- backend/src/controllers/v0/domain/sensor/sensorPostController.ts
+++ backend/src/controllers/v0/domain/sensor/sensorPostController.ts
@@ -6,13 +6,12 @@ import { SENSOR_REGISTRY_ENDPOINT } from '../../../../models/v0/serviceModels';
 import { removeServiceFromUrl } from '../../utils/api/urlUtils';
 import { fromAxiosToResponse, handleAxiosError } from '../../utils/api/responseUtils';
 import { AxiosError } from 'axios';
+import { AbstractService } from '../../../../service/v0/abstractService';
+import { sensorService } from './sensorConfig';
 
 Logger.useDefaults();
 
-const breaker = BreakerFactory.axiosBreakerWithDefaultOptions();
-const sensorService = new SensorService(breaker, SENSOR_REGISTRY_ENDPOINT);
-
-const sensorPostHandler = async (request: Request, response: Response) => {
+const sensorPostHandler = async (request: Request, response: Response, service: AbstractService<any, any>) => {
     const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
     try {
         const axiosResponse = await sensorService.registerOperation(endpointPath, request.headers, request.body);
