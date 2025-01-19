19aad90fd11deac3d63f51bdadb1025c8422815e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 17:00:57 +0100

    feat: add logging

--- backend/src/controllers/v0/domain/sensor/sensorGetController.ts
+++ backend/src/controllers/v0/domain/sensor/sensorGetController.ts
@@ -4,14 +4,19 @@ import { SENSOR_REGISTRY_ENDPOINT } from '../../../../models/v0/serviceModels';
 import { sensorService } from './sensorConfig';
 import { fromAxiosToResponse, handleAxiosError } from '../../utils/api/responseUtils';
 import { AxiosError, HttpStatusCode } from 'axios';
+import Logger from 'js-logger';
+
+Logger.useDefaults();
 
 const sensorGetHandler = async (request: Request, response: Response) => {
     const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
     try {
+        Logger.info('Requested to get all the sensors');
         const axiosResponse = await sensorService.getAllSensorsOperation(endpointPath, request.headers, request.body);
         response = fromAxiosToResponse(axiosResponse, response);
         response.send(axiosResponse.data);
     } catch (error) {
+        Logger.info('Error while trying to return all the different sensors');
         if (error instanceof AxiosError) {
             response = handleAxiosError(error, response);
         } else if (error instanceof Error) {
--- backend/src/controllers/v0/domain/sensor/sensorPostController.ts
+++ backend/src/controllers/v0/domain/sensor/sensorPostController.ts
@@ -1,12 +1,9 @@
 import { Request, Response } from 'express';
 import Logger from 'js-logger';
-import { SensorService } from '../../../../service/v0/sensor/sensorService';
-import { BreakerFactory } from '../../utils/circuitBreaker/circuitRequest';
 import { SENSOR_REGISTRY_ENDPOINT } from '../../../../models/v0/serviceModels';
 import { removeServiceFromUrl } from '../../utils/api/urlUtils';
 import { fromAxiosToResponse, handleAxiosError } from '../../utils/api/responseUtils';
-import { AxiosError } from 'axios';
-import { AbstractService } from '../../../../service/v0/abstractService';
+import { AxiosError, HttpStatusCode } from 'axios';
 import { sensorService } from './sensorConfig';
 
 Logger.useDefaults();
@@ -14,12 +11,16 @@ Logger.useDefaults();
 const sensorPostHandler = async (request: Request, response: Response) => {
     const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
     try {
+        Logger.info('Received a request for registering a new sensor');
         const axiosResponse = await sensorService.registerOperation(endpointPath, request.headers, request.body);
         response = fromAxiosToResponse(axiosResponse, response);
         response.send(axiosResponse.data);
     } catch (error) {
+        Logger.error('Error while trying to save a new sensor');
         if (error instanceof AxiosError) {
             response = handleAxiosError(error, response);
+        } else if (error instanceof Error) {
+            response.status(HttpStatusCode.BadRequest).send(error.message);
         }
     } finally {
         response.end();
