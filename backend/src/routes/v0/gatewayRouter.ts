fdb009e1b6082a236525fadedc5a4ecccf2c6c33
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 15:29:19 +0100

    feat: use sensor router

--- backend/src/routes/v0/gatewayRouter.ts
+++ backend/src/routes/v0/gatewayRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { authenticationRouter } from './authentication/authenticationRouter';
+import { sensorRouter } from './sensor/sensorRouter';
 
 const gatewayRouter = express.Router();
 
 gatewayRouter.use(authenticationRouter);
+gatewayRouter.use(sensorRouter);
 
 export { gatewayRouter };
