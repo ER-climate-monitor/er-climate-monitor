d7ed3493663211c5f322be4f46407f742ab7fc96
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 15:03:56 +0100

    feat: init Sensor Service

--- backend/src/service/sensor/sensorService.ts
+++ backend/src/service/sensor/sensorService.ts
@@ -1,5 +1,29 @@
+import { AbstractService } from "../abstractService"
+import { HttpClient } from "../../controllers/v0/utils/circuitBreaker/http/httpClient"
+import { CircuitBreakerClient } from "../../controllers/v0/utils/circuitBreaker/circuitRequest";
+
 interface SensorOperations<X> {
     registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
     getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
     deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
 }
+
+class SensorService<T extends HttpClient<X>, X> extends AbstractService<T, X> implements SensorOperations<X> {
+    constructor(circuitBreaker: CircuitBreakerClient<T, X>, endpoint: string) {
+        super(circuitBreaker, endpoint);
+    }
+
+    registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<X> {
+        throw new Error("TODO");
+    }
+
+    getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<X> {
+        throw new Error("TODO");
+    }
+
+    deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<X> {
+        throw new Error("TODO");
+    }
+}
+
+export { SensorService }
