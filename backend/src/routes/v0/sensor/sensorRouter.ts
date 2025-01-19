29d36347e0bd20e0306c5e4c405a6e1c70423cbf
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 16:40:59 +0100

    feat: add get handler

--- backend/src/routes/v0/sensor/sensorRouter.ts
+++ backend/src/routes/v0/sensor/sensorRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { SENSOR_REGISTRY_PATHS } from '../paths/gatewayPaths';
 import { sensorPostHandler } from '../../../controllers/v0/domain/sensor/sensorPostController';
+import { sensorGetHandler } from '../../../controllers/v0/domain/sensor/sensorGetController';
 
 const sensorRouter = express.Router();
 
-sensorRouter.route(SENSOR_REGISTRY_PATHS).post(sensorPostHandler);
+sensorRouter.route(SENSOR_REGISTRY_PATHS).post(sensorPostHandler).get(sensorGetHandler);
 
 export { sensorRouter };
