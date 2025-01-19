7f4a99461f2de91b5a567a394611d89847e6e9be
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 15:13:15 +0100

    refactor: change endpoint variable name

--- backend/src/service/abstractService.ts
+++ backend/src/service/abstractService.ts
@@ -2,11 +2,11 @@ import { HttpClient } from "../controllers/v0/utils/circuitBreaker/http/httpClie
 import { CircuitBreakerClient } from "../controllers/v0/utils/circuitBreaker/circuitRequest";
 
 abstract class AbstractService<T extends HttpClient<X>, X> {
-    protected authenticationEndpoint: string;
+    protected endpoint: string;
     protected circuitBreaker: CircuitBreakerClient<T, X>;
 
     constructor(circuitBreaker: CircuitBreakerClient<T, X>, endpoint: string) {
-        this.authenticationEndpoint = endpoint;
+        this.endpoint= endpoint;
         this.circuitBreaker = circuitBreaker;
     }
 }
