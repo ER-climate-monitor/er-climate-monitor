bb6e3cd5a5e20d83afffe87002e97f3225440335
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 16:41:49 +0100

    feat: init get handler

---
+++ backend/src/controllers/v0/domain/sensor/sensorGetController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { removeServiceFromUrl } from '../../utils/api/urlUtils';
+import { SENSOR_REGISTRY_ENDPOINT } from '../../../../models/v0/serviceModels';
+import { sensorService } from './sensorConfig';
+import { fromAxiosToResponse, handleAxiosError } from '../../utils/api/responseUtils';
+import { AxiosError, HttpStatusCode } from 'axios';
+
+const sensorGetHandler = async (request: Request, response: Response) => {
+    const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
+    try {
+        const axiosResponse = await sensorService.getAllSensorsOperation(endpointPath, request.headers, request.body);
+        response = fromAxiosToResponse(axiosResponse, response);
+        response.send(axiosResponse.data);
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            response = handleAxiosError(error, response);
+        } else if (error instanceof Error) {
+            response.status(HttpStatusCode.BadRequest).send(error.message);
+        }
+    } finally {
+        response.end();
+    }
+};
+
+export { sensorGetHandler };
