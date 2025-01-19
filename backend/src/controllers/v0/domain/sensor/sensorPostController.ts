1da9511a9899db1ac9288e64ba27f426fe7694a0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-19 14:43:03 +0100

    feat: init post controller

---
+++ backend/src/controllers/v0/domain/sensor/sensorPostController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import Logger from 'js-logger';
+
+Logger.useDefaults();
+
+const sensorPostHandler = async (request: Request, response: Response) => {};
