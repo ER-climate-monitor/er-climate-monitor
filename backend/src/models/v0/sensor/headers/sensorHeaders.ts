e836aff5dc2b8a7b03a98d3cd5e58409b55a8b2d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-20 10:12:26 +0100

    feat: add actions

--- backend/src/models/v0/sensor/headers/sensorHeaders.ts
+++ backend/src/models/v0/sensor/headers/sensorHeaders.ts
@@ -1,5 +1,9 @@
 const SENSOR_IP_BODY = 'sensorIp';
 const SENSOR_PORT_BODY = 'sensorPort';
 const API_KEY_BODY = 'apiKey';
+const SENSOR_ACTION = "action";
 
-export { SENSOR_IP_BODY, SENSOR_PORT_BODY, API_KEY_BODY };
+const REGISTER_ACTION = "register";
+const RETURN_ALL_ACTION = "returnAll";
+
+export { SENSOR_IP_BODY, SENSOR_PORT_BODY, API_KEY_BODY, REGISTER_ACTION, RETURN_ALL_ACTION };
