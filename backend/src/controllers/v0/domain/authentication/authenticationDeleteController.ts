caddbbadf419f25d2887730cc6ebad1f24b8300d
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-20 10:11:50 +0100

    feat: add more checks inside the catch

--- backend/src/controllers/v0/domain/authentication/authenticationDeleteController.ts
+++ backend/src/controllers/v0/domain/authentication/authenticationDeleteController.ts
@@ -3,7 +3,7 @@ import { AUTHENTICATION_SERVICE } from '../../../../routes/v0/paths/gatewayPaths
 import { fromAxiosToResponse, handleAxiosError } from '../../utils/api/responseUtils';
 import { Request, Response } from 'express';
 import Logger from 'js-logger';
-import { AxiosError } from 'axios';
+import { AxiosError, HttpStatusCode } from 'axios';
 import { authenticationService } from './authenticationConfig';
 
 Logger.useDefaults();
@@ -11,17 +11,20 @@ Logger.useDefaults();
 const authenticationDeleteHandler = async (request: Request, response: Response) => {
     const endpointPath = removeServiceFromUrl(AUTHENTICATION_SERVICE, request.url);
     try {
+        Logger.info('Requested to delete a user');
         const axiosResponse = await authenticationService.deleteOperation(
             endpointPath,
             request.headers,
-            request.headers,
+            request.body,
         );
         response = fromAxiosToResponse(axiosResponse, response);
         response.send(axiosResponse.data);
     } catch (error) {
         Logger.error('Error during delete operation.');
-        if (error instanceof AxiosError) {
+        if (error instanceof AxiosError && error.request !== null) {
             response = handleAxiosError(error, response);
+        } else if (error instanceof Error) {
+            response.status(HttpStatusCode.BadRequest).send(error.message);
         }
     } finally {
         response.end();
