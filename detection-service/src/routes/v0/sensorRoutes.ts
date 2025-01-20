ef4725654bf521e5036e672b6876c7b4a5013568
S-furi <stefano.furi@studio.unibo.it>
2025-01-20 12:22:41 +0100

    refactor(detections): collapse router endpoint regitration call into single call for same URL

--- detection-service/src/routes/v0/sensorRoutes.ts
+++ detection-service/src/routes/v0/sensorRoutes.ts
@@ -8,14 +8,15 @@ import { API_ROUTES } from './paths/detection.paths';
 import cors from 'cors';
 
 const router = express.Router();
-router.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+router.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+);
 
-router.route(API_ROUTES.SENSOR.DETECTIONS).post(saveDetection);
-router.route(API_ROUTES.SENSOR.DETECTIONS).get(getDetectionsFromSensor);
+router.route(API_ROUTES.SENSOR.DETECTIONS).post(saveDetection).get(getDetectionsFromSensor);
 router.route(API_ROUTES.SENSOR.POSITIONS).get(getSensorLocationsByType);
 
 export default router;
