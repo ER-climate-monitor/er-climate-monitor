4eb17cd0b05b92d2f84a189409212ea2f8f71297
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-20 10:28:40 +0100

    feat: add email and role inside token infos

--- authentication-service/src/models/v0/tokenModel.ts
+++ authentication-service/src/models/v0/tokenModel.ts
@@ -1,14 +1,20 @@
 interface IToken {
     token: string,
+    email: string,
+    role: string,
     expiration: Date
 }
 
 class Token implements IToken {
     token: string;
+    email: string;
+    role: string;
     expiration: Date;
 
-    constructor(token: string, expiration: Date) {
+    constructor(token: string, email: string, role: string, expiration: Date) {
         this.token = token;
+        this.email = email;
+        this.role = role;
         this.expiration = expiration;
     }
 }
