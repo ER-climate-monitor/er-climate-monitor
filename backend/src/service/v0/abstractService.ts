5ad98259c9d0d47bec243fbab7927ada0b59f50e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-20 15:26:02 +0100

    feat: add authentication client inside the abstract service

--- backend/src/service/v0/abstractService.ts
+++ backend/src/service/v0/abstractService.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '../../controllers/v0/utils/circuitBreaker/http/httpClient';
 import { CircuitBreakerClient } from '../../controllers/v0/utils/circuitBreaker/circuitRequest';
+import { IAuthenticationClient } from '../../controllers/v0/utils/redis/redisClient';
 
 abstract class AbstractService<T extends HttpClient<X>, X> {
     protected endpoint: string;
     protected circuitBreaker: CircuitBreakerClient<T, X>;
+    authenticationClient: IAuthenticationClient;
 
-    constructor(circuitBreaker: CircuitBreakerClient<T, X>, endpoint: string) {
+    constructor(circuitBreaker: CircuitBreakerClient<T, X>, endpoint: string, authenticationClient: IAuthenticationClient) {
         this.endpoint = endpoint;
         this.circuitBreaker = circuitBreaker;
+        this.authenticationClient = authenticationClient;
     }
 }
 
