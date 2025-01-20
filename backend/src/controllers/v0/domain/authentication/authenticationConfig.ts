fc37b733b24ad6fcc4a3b839c11a9ba34aa26476
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-20 15:26:43 +0100

    refactor: add authentication Redis client inside the authentication service constructor

--- backend/src/controllers/v0/domain/authentication/authenticationConfig.ts
+++ backend/src/controllers/v0/domain/authentication/authenticationConfig.ts
@@ -1,8 +1,9 @@
 import { BreakerFactory } from '../../utils/circuitBreaker/circuitRequest';
 import { AuthenticationService } from '../../../../service/v0/authentication/authenticationService';
 import { AUTHENTICATION_ENDPOINT } from '../../../../models/v0/serviceModels';
-
+import { authenticationRedisClient } from '../../utils/redis/redisClient';
 const breaker = BreakerFactory.axiosBreakerWithDefaultOptions();
-const authenticationService = new AuthenticationService(breaker, AUTHENTICATION_ENDPOINT);
+
+const authenticationService = new AuthenticationService(breaker, AUTHENTICATION_ENDPOINT, authenticationRedisClient);
 
 export { authenticationService };
