ca7921f02e4cdcab4f990a4995b349ec49820ab3
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-21 14:56:29 +0100

    feat: add function for retreiving a value from all the headers

--- sensor-registry/src/controllers/v0/utils/requestUtils.ts
+++ sensor-registry/src/controllers/v0/utils/requestUtils.ts
@@ -2,4 +2,8 @@ function fromBody<X>(body: any, key: string, defaultValue: X): X {
     return body && key in body ? body[key] : defaultValue;
 }
 
-export { fromBody };
+function fromHeaders<X>(headers: any, key: string, defaultValue: X): X {
+    return headers && key in headers ? headers[key] : defaultValue;
+}
+
+export { fromBody, fromHeaders };
