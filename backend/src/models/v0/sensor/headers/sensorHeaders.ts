f99b3482a217edaaf3e2213c80a94090ce92c3ea
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-21 16:18:45 +0100

    refactor: change const name

--- backend/src/models/v0/sensor/headers/sensorHeaders.ts
+++ backend/src/models/v0/sensor/headers/sensorHeaders.ts
@@ -1,9 +1,9 @@
 const SENSOR_IP_BODY = 'sensorIp';
 const SENSOR_PORT_BODY = 'sensorPort';
-const API_KEY_BODY = 'apiKey';
+const API_KEY_HEADER = 'apiKey';
 const SENSOR_ACTION = "action";
 
 const REGISTER_ACTION = "register";
 const RETURN_ALL_ACTION = "returnAll";
 
-export { SENSOR_IP_BODY, SENSOR_PORT_BODY, API_KEY_BODY, REGISTER_ACTION, RETURN_ALL_ACTION };
+export { SENSOR_IP_BODY, SENSOR_PORT_BODY, API_KEY_HEADER, REGISTER_ACTION, RETURN_ALL_ACTION };
