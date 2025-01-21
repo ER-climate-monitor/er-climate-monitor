3519dd5a0831ad7b6cc77ad158641d8ee3026271
Fabio Vincenzi <fabio.vincenzi2001@gmail.com>
2025-01-21 17:33:25 +0100

    refactor: export server code into server.ts

--- detection-service/src/index.ts
+++ detection-service/src/index.ts
@@ -1,36 +1,23 @@
-import express, { Application } from 'express';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-// import SwaggerUi  from "swagger-ui-express";
-import sensorRouter from './routes/v0/sensorRoutes';
-import { API_ROUTES } from './routes/v0/paths/detection.paths';
-// import YAML from "yaml";
-import cors from 'cors';
-dotenv.config();
+import createServer from './server';
+import dotenv from 'dotenv';
 
 const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || '';
 
-export default function createServer(): Application {
-    const app = express();
-    const URL: string = process.env.DB_URL || '';
-    mongoose.connect("mongodb+srv://fabiovincenzi2001:Gszz1ORmQ7QwOigJ@cluster0.qd8ak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: 'detections-database', autoIndex: false });
+async function startServer() {
+    try {
+        dotenv.config();
+        await mongoose.connect(DB_URL, { dbName: 'detections-database', autoIndex: false });
+        console.log('Connected to MongoDB');
 
-    app.options('*', cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    // if (!process.env.CI || (process.env.CI == "False")) {
-    //     const file: string = fs.readFileSync("src/doc/openapi/swagger.yaml", "utf8");
-    //     const swaggerDocument = YAML.parse(file);
-    //     app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
-    // }
-
-    app.use(API_ROUTES.SENSOR.ROOT, sensorRouter);
-    
-    return app;
+        const app = createServer();
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+    }
 }
 
-const app = createServer();
-
-app.listen(PORT, () => {
-    console.log('Server listening on port', PORT);
-});
+startServer();
