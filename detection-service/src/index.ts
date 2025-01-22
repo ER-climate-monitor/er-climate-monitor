aaad65197185bc0f35579fd6428b3c7e90795173
S-furi <stefano.furi@studio.unibo.it>
2025-01-22 19:16:55 +0100

    fix: connect broker when starting server

--- detection-service/src/index.ts
+++ detection-service/src/index.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 import createServer from './server';
 import dotenv from 'dotenv';
+import { detectionPublisher } from './controllers/v0/utils/brokerClient';
+import Logger from 'js-logger';
 
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DB_URL || '';
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
 
 async function startServer() {
     try {
@@ -11,6 +13,10 @@ async function startServer() {
         await mongoose.connect(DB_URL, { dbName: 'detections-database', autoIndex: false });
         console.log('Connected to MongoDB');
 
+        detectionPublisher.connect();
+        console.log('Connected to Notification Event Broker!');
+
+        Logger.useDefaults();
         const app = createServer();
         app.listen(PORT, () => {
             console.log(`Server listening on port ${PORT}`);
