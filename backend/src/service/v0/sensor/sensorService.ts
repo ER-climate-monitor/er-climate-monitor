94cd8c580e7d78a2d5727276b12ade090f18f30e
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-22 12:03:26 +0100

    refactor: remove generic from htp client and use http response

--- backend/src/service/v0/sensor/sensorService.ts
+++ backend/src/service/v0/sensor/sensorService.ts
@@ -3,31 +3,32 @@ import { HttpClient } from '../../../controllers/v0/utils/circuitBreaker/http/ht
 import { CircuitBreakerClient } from '../../../controllers/v0/utils/circuitBreaker/circuitRequest';
 import { DELETE, GET, POST } from '../../../controllers/v0/utils/api/httpMethods';
 import { IAuthenticationClient } from '../../../controllers/v0/utils/redis/redisClient';
+import { HttpResponse } from '../../../controllers/v0/utils/circuitBreaker/http/httpResponse';
 
-interface SensorOperations<X> {
-    registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
-    getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
-    deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<X>;
+interface SensorOperations {
+    registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
+    getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
+    deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
 }
 
-class SensorService<T extends HttpClient<X>, X> extends AbstractService<T, X> implements SensorOperations<X> {
+class SensorService<T extends HttpClient> extends AbstractService<T> implements SensorOperations {
     constructor(
-        circuitBreaker: CircuitBreakerClient<T, X>,
+        circuitBreaker: CircuitBreakerClient<T>,
         endpoint: string,
         authenticationClient: IAuthenticationClient,
     ) {
         super(circuitBreaker, endpoint, authenticationClient);
     }
 
-    registerOperation(endpointPath: string, headers: any, body: any): Promise<X> {
+    registerOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
         return this.circuitBreaker.fireRequest(this.endpoint, POST, endpointPath, headers, body);
     }
 
-    getAllSensorsOperation(endpointPath: string, headers: any, body: any): Promise<X> {
+    getAllSensorsOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
         return this.circuitBreaker.fireRequest(this.endpoint, GET, endpointPath, headers, body);
     }
 
-    deleteOperation(endpointPath: string, headers: any, body: any): Promise<X> {
+    deleteOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
         return this.circuitBreaker.fireRequest(this.endpoint, DELETE, endpointPath, headers, body);
     }
 }
