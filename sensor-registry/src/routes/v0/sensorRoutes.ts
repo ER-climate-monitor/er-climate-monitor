db73a9d58c1c7c3a5453a4ce80d943c4ed0514c6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-22 17:27:43 +0100

    merge: solve conflicts

--- sensor-registry/src/routes/v0/sensorRoutes.ts
+++ sensor-registry/src/routes/v0/sensorRoutes.ts
@@ -6,14 +6,14 @@ import {
     allSensorsInfo,
     allSensorsOfType,
 } from '../../controllers/v0/sensorController';
-import { ALL_PATH, REGISTER_PATH, SHUT_OFF_PATH, ALL_INFO_ROUTE, TYPE_PATH } from './paths/sensorPaths';
+import { ALL_PATH, REGISTER_PATH, SHUT_DOWN_ROUTE, ALL_INFO_ROUTE, TYPE_PATH } from './paths/sensorPaths';
 const sensorRouter = express.Router();
 
 sensorRouter.route(REGISTER_PATH).post(registerSensor);
 
 sensorRouter.route(ALL_PATH).get(allSensors);
 
-sensorRouter.route(SHUT_DOWN_PATH).delete(shutDown);
+sensorRouter.route(SHUT_DOWN_ROUTE).delete(shutDown);
 
 sensorRouter.route(ALL_INFO_ROUTE).get(allSensorsInfo);
 
