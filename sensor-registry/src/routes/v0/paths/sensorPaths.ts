59b04e356fae6ceb8ce05710b4a4e52eda2ed1df
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-23 10:52:34 +0100

    feat: init path

--- sensor-registry/src/routes/v0/paths/sensorPaths.ts
+++ sensor-registry/src/routes/v0/paths/sensorPaths.ts
@@ -12,6 +12,8 @@ const ALL_INFO_ROUTE = BASE_SENSOR_PATH_V0 + ALL_PATH + ALL_INFO_PATH;
 const SHUT_DOWN_PATH = '/shutdown';
 const SHUT_DOWN_ROUTE = BASE_SENSOR_PATH_V0 + SHUT_DOWN_PATH;
 
+const UPDATE_SENSOR_NAME_PATH = '/'
+
 const TYPE_PATH = '/type';
 
 export {
