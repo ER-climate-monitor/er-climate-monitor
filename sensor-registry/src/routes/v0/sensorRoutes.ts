2f8d1c975447fc7d21563cdbe2d4845a701519e3
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-23 16:21:21 +0100

    fix: use correct shut down path

--- sensor-registry/src/routes/v0/sensorRoutes.ts
+++ sensor-registry/src/routes/v0/sensorRoutes.ts
@@ -6,14 +6,14 @@ import {
     allSensorsInfo,
     allSensorsOfType,
 } from '../../controllers/v0/sensorController';
-import { ALL_PATH, REGISTER_PATH, SHUT_DOWN_ROUTE, TYPE_PATH, ALL_INFO_PATH } from './paths/sensorPaths';
+import { ALL_PATH, REGISTER_PATH, SHUT_DOWN_PATH, TYPE_PATH, ALL_INFO_PATH } from './paths/sensorPaths';
 const sensorRouter = express.Router();
 
 sensorRouter.route(REGISTER_PATH).post(registerSensor);
 
 sensorRouter.route(ALL_PATH).get(allSensors);
 
-sensorRouter.route(SHUT_DOWN_ROUTE).delete(shutDown);
+sensorRouter.route(SHUT_DOWN_PATH).delete(shutDown);
 
 sensorRouter.route(ALL_INFO_PATH).get(allSensorsInfo);
 
