7b20f3cd55bbe140582918f49554dbe92f81a876
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-23 16:53:06 +0100

    feat: create functions for configuring the application

---
+++ authentication-service/src/appUtils.ts
@@ -0,0 +1,49 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import { userRouter } from './routes/v0/userRouter';
+import { healthRouter } from './routes/v0/healthRouter';
+import mongoose from 'mongoose';
+import SwaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+function configureServer() {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    if (!process.env.CI || process.env.CI == 'False') {
+        const file: string = fs.readFileSync('src/doc/openapi/swagger.yaml', 'utf8');
+        const swaggerDocument = YAML.parse(file);
+        app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
+    }
+
+    app.use('/v0/health', healthRouter);
+    app.use('/v0/user', userRouter);
+    return app;
+}
+
+function createProdServer(url: string) {
+    mongoose.connect(url, { dbName: 'authorization-database' });
+    return configureServer(); 
+}
+
+function createTestServer(): Application {
+    const url: string =  String(process.env.TEST_DB_URL) || 'mongodb://localhost:27017/';
+    mongoose.connect(url, { dbName: 'authorization-database' });
+    return configureServer();
+}
+
+async function dropTestDatabase() {
+    const url: string =  String(process.env.TEST_DB_URL) || 'mongodb://localhost:27017/';
+    mongoose.connect(url, { dbName: 'sensor-database' })
+        .then((mongodb) => {
+            mongodb.connection.dropDatabase();
+        });
+}
+
+export { createProdServer, createTestServer, dropTestDatabase }
