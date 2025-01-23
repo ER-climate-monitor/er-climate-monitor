b37b96f08ad23601c85bfd743e310e0b9b238192
S-furi <stefano.furi@studio.unibo.it>
2025-01-23 18:48:23 +0100

    feat: add WebSocketProxy forwarder for websocket incoming/outgoing messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,13 @@ import dotenv from 'dotenv';
 import { BASE_PATH_V0 } from './routes/v0/paths/gatewayPaths';
 import { gatewayRouter } from './routes/v0/gatewayRouter';
 import cors from 'cors';
+import { createServer } from 'http';
+import { WebSocketProxy } from './service/v0/notification/WebSocketProxy';
+import Logger from 'js-logger';
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -16,5 +20,15 @@ app.get(BASE_PATH_V0, (_, res) => {
 });
 
 app.use(BASE_PATH_V0, gatewayRouter);
+const wsProxy = new WebSocketProxy(httpServer);
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+httpServer.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
+process.on('SIGINT', () => {
+    Logger.info('Shutting down server...');
+    wsProxy.close();
+    httpServer.close(() => {
+        Logger.info('HTTP server closed successfully, bye.');
+        process.exit(0);
+    });
+});
