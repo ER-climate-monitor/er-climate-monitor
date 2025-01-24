de5d357d4b739d3f05dec06b7f81f638c44f2dcd
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 12:02:55 +0100

    feat: get the token from the headers

--- backend/src/controllers/v0/domain/sensor/sensorGetController.ts
+++ backend/src/controllers/v0/domain/sensor/sensorGetController.ts
@@ -3,7 +3,7 @@ import { removeServiceFromUrl } from '../../utils/api/urlUtils';
 import { SENSOR_REGISTRY_ENDPOINT } from '../../../../models/v0/serviceModels';
 import { sensorService } from './sensorConfig';
 import Logger from 'js-logger';
-import { USER_JWT_TOKEN_BODY } from '../../../../models/v0/authentication/headers/authenticationHeaders';
+import { USER_TOKEN_HEADER } from '../../../../models/v0/authentication/headers/authenticationHeaders';
 import HttpStatus from 'http-status-codes';
 import { API_KEY_HEADER } from '../../../../models/v0/sensor/headers/sensorHeaders';
 import { fromHttpResponseToExpressResponse } from '../../utils/api/responseUtils';
@@ -15,11 +15,12 @@ const sensorGetHandler = async (request: Request, response: Response) => {
     let endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
     try {
         Logger.info('Requested to get all the sensors');
-        const jwtToken = String(request.headers[USER_JWT_TOKEN_BODY.toLowerCase()]);
+        const jwtToken = String(request.headers[USER_TOKEN_HEADER.toLowerCase()]);
 
         const isExpired = await sensorService.authenticationClient.isExpired(jwtToken);
 
         if (isExpired) {
+            Logger.info('The token is expired');
             response.status(HttpStatus.UNAUTHORIZED);
             return;
         }
