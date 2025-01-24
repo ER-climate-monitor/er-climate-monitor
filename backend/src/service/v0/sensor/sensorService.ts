31d850bf06b7104cafd20eed9627b7bec99cd720
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 15:00:08 +0100

    feat: add update sensor operation inside the sensor registry class

--- backend/src/service/v0/sensor/sensorService.ts
+++ backend/src/service/v0/sensor/sensorService.ts
@@ -1,7 +1,7 @@
 import { AbstractService } from '../abstractService';
 import { HttpClient } from '../../../controllers/v0/utils/circuitBreaker/http/httpClient';
 import { CircuitBreakerClient } from '../../../controllers/v0/utils/circuitBreaker/circuitRequest';
-import { DELETE, GET, POST } from '../../../controllers/v0/utils/api/httpMethods';
+import { DELETE, GET, POST, PUT } from '../../../controllers/v0/utils/api/httpMethods';
 import { IAuthenticationClient } from '../../../controllers/v0/utils/redis/redisClient';
 import { HttpResponse } from '../../../controllers/v0/utils/circuitBreaker/http/httpResponse';
 
@@ -9,6 +9,7 @@ interface SensorOperations {
     registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
     getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
     deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
+    updateRemoteSensor(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
 }
 
 class SensorService<T extends HttpClient> extends AbstractService<T> implements SensorOperations {
@@ -30,6 +31,10 @@ class SensorService<T extends HttpClient> extends AbstractService<T> implements 
 
     deleteOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
         return this.circuitBreaker.fireRequest(this.endpoint, DELETE, endpointPath, headers, body);
+    }
+
+    updateRemoteSensor(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
+        return this.circuitBreaker.fireRequest(this.endpoint, PUT, endpointPath, headers, body);
     }
 }
 
