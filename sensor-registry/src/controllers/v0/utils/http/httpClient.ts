502167406e6deddec60c339d8f90e96fa9fc78b3
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 16:45:01 +0100

    feat: add update cronjob time logic

--- sensor-registry/src/controllers/v0/utils/http/httpClient.ts
+++ sensor-registry/src/controllers/v0/utils/http/httpClient.ts
@@ -1,11 +1,11 @@
-import axios, { HttpStatusCode } from 'axios';
-import { SHUT_DOWN_PATH } from "../../../../routes/v0/paths/sensorPaths";
-import { SENSOR_CRONJOB_DAYS, SENSOR_NAME } from '../../../../model/v0/headers/sensorHeaders';
+import axios from 'axios';
+import { SENSOR_CRONJOB_DAYS, SENSOR_CRONJOB_TIME_HOUR, SENSOR_CRONJOB_TIME_MINUTE, SENSOR_NAME } from '../../../../model/v0/headers/sensorHeaders';
 
 interface HttpClient {
     deleteSensor(endpoint: string, ip: string, port: number): Promise<boolean>;
     updateSensorName(endpoint: string, ip: string, port: number, newName: string): Promise<boolean>;
     updateCronJobDays(endpoint: string, ip: string, port: number, newDays: string): Promise<boolean>;
+    updateCronJobTime(endpoint: string, ip: string, port: number, hour: string, minute: string): Promise<boolean>;
 }
 
 class BasicHttpClient implements HttpClient{
@@ -28,6 +28,11 @@ class BasicHttpClient implements HttpClient{
     async updateCronJobDays(endpoint: string, ip: string, port: number, newDays: string): Promise<boolean> {
         const url = this.makeURL(ip, port, endpoint);
         return axios.put(url, {[SENSOR_CRONJOB_DAYS]: newDays});
+    }
+
+    async updateCronJobTime(endpoint: string, ip: string, port: number, hour: string, minute: string): Promise<boolean> {
+        const url = this.makeURL(ip, port, endpoint);
+        return axios.put(url, {[SENSOR_CRONJOB_TIME_HOUR]: hour, [SENSOR_CRONJOB_TIME_MINUTE]: minute});
     }
 }
 
