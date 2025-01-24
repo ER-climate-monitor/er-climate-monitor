37944e693a6d2277658b1b9d325868597a4d261f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 15:17:42 +0100

    feat: add physical sensors paths

---
+++ sensor-registry/src/routes/v0/paths/physicalSensorPaths.ts
@@ -0,0 +1,6 @@
+const UPDATE_SENSOR_NAME_PATH = '/sensor/update/name';
+const UPDATE_SENSOR_CRONJOB_DAYS_PATH = '/sensor/configuration/cron/days';
+const UPDATE_SENSOR_CRONJOB_TIME_PATH = '/sensor/configuration/cron/time';
+const SHUTDOWN_SENSOR_PATH = '/shutdown';
+
+export {UPDATE_SENSOR_CRONJOB_DAYS_PATH, UPDATE_SENSOR_CRONJOB_TIME_PATH, UPDATE_SENSOR_NAME_PATH, SHUTDOWN_SENSOR_PATH}
