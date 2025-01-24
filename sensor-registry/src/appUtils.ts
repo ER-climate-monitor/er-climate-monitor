ceef59c680cab4f78993a202db340203e11f4bb6
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 17:04:31 +0100

    feat: import and use swagger

--- sensor-registry/src/appUtils.ts
+++ sensor-registry/src/appUtils.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import { sensorRouter } from './routes/v0/sensorRoutes';
 import mongoose from 'mongoose';
 import { BASE_SENSOR_PATH_V0 } from './routes/v0/paths/sensorPaths';
-import cors from 'cors';
+import SwaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
 
 dotenv.config();
 
@@ -12,6 +14,11 @@ function createServer(url: string): Application {
     mongoose.connect(url, { dbName: 'sensor-database' });
     app.use(express.json());
     app.use(BASE_SENSOR_PATH_V0, sensorRouter);
+    if (!process.env.CI || process.env.CI == 'False') {
+        const file: string = fs.readFileSync('src/doc/openapi/swagger.yaml', 'utf8');
+        const swaggerDocument = YAML.parse(file);
+        app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
+    }
     return app;
 }
 
