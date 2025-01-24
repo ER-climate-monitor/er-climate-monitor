dbe2008f514917ea6f9911f27cbaceceaee61400
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 12:06:21 +0100

    feat: get the user token from the headers

--- backend/src/controllers/v0/domain/sensor/sensorDeleteController.ts
+++ backend/src/controllers/v0/domain/sensor/sensorDeleteController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { sensorService } from './sensorConfig';
-import { USER_JWT_TOKEN_BODY } from '../../../../models/v0/authentication/headers/authenticationHeaders';
+import { USER_JWT_TOKEN_BODY, USER_TOKEN_HEADER } from '../../../../models/v0/authentication/headers/authenticationHeaders';
 import { API_KEY_HEADER } from '../../../../models/v0/sensor/headers/sensorHeaders';
 import Logger from 'js-logger';
 import HttpStatus from 'http-status-codes';
@@ -15,7 +15,7 @@ const sensorDeleteHandler = async (request: Request, response: Response) => {
     try {
         Logger.info('Received a request for deleting a sensor');
         const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
-        const jwtToken = String(request.headers[USER_JWT_TOKEN_BODY.toLowerCase()]);
+        const jwtToken = String(request.headers[USER_TOKEN_HEADER.toLowerCase()]);
         const authorized = (await sensorService.authenticationClient.isAdminAndNotExpired(jwtToken));
         if (jwtToken === null || !authorized) {
             response.status(HttpStatus.UNAUTHORIZED);
