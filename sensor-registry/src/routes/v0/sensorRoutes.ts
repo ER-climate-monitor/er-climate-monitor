c436a159cb15ce6027fce1cae19fd5c07408be34
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 15:19:12 +0100

    feat: export and use the new path for updating the sensor

--- sensor-registry/src/routes/v0/sensorRoutes.ts
+++ sensor-registry/src/routes/v0/sensorRoutes.ts
@@ -5,8 +5,9 @@ import {
     shutDown,
     allSensorsInfo,
     allSensorsOfType,
+    updateSensorInfo,
 } from '../../controllers/v0/sensorController';
-import { ALL_PATH, REGISTER_PATH, SHUT_DOWN_PATH, TYPE_PATH, ALL_INFO_PATH } from './paths/sensorPaths';
+import { ALL_PATH, REGISTER_PATH, SHUT_DOWN_PATH, TYPE_PATH, ALL_INFO_PATH, UPDATE_SENSOR_PATH } from './paths/sensorPaths';
 const sensorRouter = express.Router();
 
 sensorRouter.route(REGISTER_PATH).post(registerSensor);
@@ -18,5 +19,7 @@ sensorRouter.route(SHUT_DOWN_PATH).delete(shutDown);
 sensorRouter.route(ALL_INFO_PATH).get(allSensorsInfo);
 
 sensorRouter.route(TYPE_PATH).get(allSensorsOfType);
+
+sensorRouter.route(UPDATE_SENSOR_PATH).put(updateSensorInfo)
 
 export { sensorRouter };
