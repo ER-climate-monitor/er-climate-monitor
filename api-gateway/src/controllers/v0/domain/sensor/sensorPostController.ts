fec0082e5ba15fba285bd879fc2252c48add02b8
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-26 17:14:59 +0100

    feat: add check before registering a new sensor. Check also if the user is an admin and if his token is not expired

--- api-gateway/src/controllers/v0/domain/sensor/sensorPostController.ts
+++ api-gateway/src/controllers/v0/domain/sensor/sensorPostController.ts
@@ -5,13 +5,23 @@ import { removeServiceFromUrl } from '../../utils/api/urlUtils';
 import { sensorService } from './sensorConfig';
 import HttpStatus from 'http-status-codes';
 import { fromHttpResponseToExpressResponse } from '../../utils/api/responseUtils';
+import { USER_TOKEN_HEADER } from '../../../../models/v0/authentication/headers/authenticationHeaders';
+import { API_KEY_HEADER } from '../../../../models/v0/sensor/headers/sensorHeaders';
 
 Logger.useDefaults();
+const SECRET = String(process.env.SECRET_API_KEY);
+
 
 const sensorPostHandler = async (request: Request, response: Response) => {
     const endpointPath = removeServiceFromUrl(SENSOR_REGISTRY_ENDPOINT, request.url);
     try {
         Logger.info('Received a request for registering a new sensor');
+        const token = String(request.headers[USER_TOKEN_HEADER.toLowerCase()]) || '';
+        if (!sensorService.authenticationClient.isAdminAndNotExpired(token)) {
+            response.status(HttpStatus.UNAUTHORIZED);
+            return;
+        }
+        request.headers[API_KEY_HEADER] = SECRET
         const httpResponse = await sensorService.registerOperation(endpointPath, request.headers, request.body);
         response = fromHttpResponseToExpressResponse(httpResponse, response);
         response.send(httpResponse.data);
