9ba2737df305e7fe49d0a3310a07214a666b4c60
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-28 16:47:21 +0100

    doc: add doc for axios http client

--- api-gateway/src/controllers/v0/utils/circuitBreaker/http/axios/axiosClient.ts
+++ api-gateway/src/controllers/v0/utils/circuitBreaker/http/axios/axiosClient.ts
@@ -5,9 +5,12 @@ function axiosCheckServerError(error: AxiosError<unknown, any>): boolean {
     return error.status !== undefined && error.status < 500;
 }
 
+/**
+ * Axios Implementation for an Http Client.
+ */
 class AxiosHttpClient implements HttpClient {
     constructor() {}
-
+    
     private fromAxiosHeadersToRecord(axiosHeaders: any): Record<string, string> {
         const headers: Record<string, string> = {};
         Object.keys(axiosHeaders).forEach((key) => (headers[key] = axiosHeaders[key]));
