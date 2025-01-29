9a2003e9a3dfec0e32a5ee3ebb734243422ff949
S-furi <stefano.furi@studio.unibo.it>
2025-01-29 10:53:30 +0100

    fix(notifications): fix endpoint path

--- notification-service/src/notificationRoutes.ts
+++ notification-service/src/notificationRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { subscribeUser, deleteUserSubscription, getUserSubscriptions, getNotificationsForUser } from './handlers/notificationHttpHandler';
+import {
+    subscribeUser,
+    deleteUserSubscription,
+    getUserSubscriptions,
+    getNotificationsForUser,
+} from './handlers/notificationHttpHandler';
 const router = express.Router();
 
-router.route('/subscription').post(subscribeUser).delete(deleteUserSubscription).get(getUserSubscriptions);
+router.route('/subscriptions').post(subscribeUser).delete(deleteUserSubscription).get(getUserSubscriptions);
 router.route('/').get(getNotificationsForUser);
 
 export { router };
