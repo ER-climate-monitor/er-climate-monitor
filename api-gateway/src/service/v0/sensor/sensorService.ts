79f58d0dc0a2a2834b26486d7832551573894823
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-31 12:02:56 +0100

    chore: add new jsdoc and refactor some parameter typesa

--- api-gateway/src/service/v0/sensor/sensorService.ts
+++ api-gateway/src/service/v0/sensor/sensorService.ts
@@ -7,12 +7,38 @@ import { HttpResponse } from '../../../controllers/v0/utils/circuitBreaker/http/
 import { BasicHttpRequest } from '../../../controllers/v0/utils/circuitBreaker/http/httpRequest';
 
 /**
- * TODO:
+ * Group the main operations that will be redirected to the Sensor Registry Service.
  */
 interface SensorOperations {
-    registerOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
-    getAllSensorsOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
-    deleteOperation(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
+    /**
+     * Register a new input sensor.
+     * @param {string} _endpointPath - Endpoint path where the request will be redirected;
+     * @param {Record<string, string>} _headers - Input headers that will be redirected to the sensor registry;
+     * @param {object} _body - Input sensor information;
+     * @returns {Promise<HttpResponse>} Sensor registry response.
+     */
+    registerOperation(_endpointPath: string, _headers: Record<string, string>, _body: object): Promise<HttpResponse>;
+    /**
+     * Return all the different stored sensors;
+     * @param {string} _endpointPath - Endpoint path where the request will be redirected;
+     * @param {Record<string, string>} _headers - Input headers that will be redirected to the sensor registry;
+     * @returns {Promise<HttpResponse>} Sensor registry response.
+     */
+    getAllSensorsOperation(_endpointPath: string, _headers: Record<string, string>): Promise<HttpResponse>;
+    /**
+     * Delete the input sensor.
+     * @param {string} _endpointPath - Endpoint path where the request will be redirected;
+     * @param {Record<string, string>} _headers - Input headers;
+     * @returns {Promise<HttpResponse>} Sensor registry response.
+     */
+    deleteOperation(_endpointPath: string, _headers: Record<string, string>): Promise<HttpResponse>;
+    /**
+     * Update the input sensor.
+     * @param {string} _endpointPath - Endpoint path where the request will be redirected.
+     * @param {Record<string, string>} _headers - Input headers;
+     * @param {object} _body - New sensor information
+     * @returns {Promise<HttpResponse>} Sensor registry response.
+     */
     updateRemoteSensor(_endpointPath: string, _headers: any, _body: any): Promise<HttpResponse>;
 }
 
@@ -21,18 +47,18 @@ class SensorService<T extends HttpClient> extends AbstractService<T> implements 
         super(circuitBreaker, endpoint, authenticationClient);
     }
 
-    registerOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
+    registerOperation(endpointPath: string, headers: any, body: object): Promise<HttpResponse> {
         const request = new BasicHttpRequest(HttpMethods.POST, endpointPath, headers, body, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
-    getAllSensorsOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
-        const request = new BasicHttpRequest(HttpMethods.GET, endpointPath, headers, body, {}, {});
+    getAllSensorsOperation(endpointPath: string, headers: any): Promise<HttpResponse> {
+        const request = new BasicHttpRequest(HttpMethods.GET, endpointPath, headers, {}, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
-    deleteOperation(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
-        const request = new BasicHttpRequest(HttpMethods.DELETE, endpointPath, headers, body, {}, {});
+    deleteOperation(endpointPath: string, headers: any): Promise<HttpResponse> {
+        const request = new BasicHttpRequest(HttpMethods.DELETE, endpointPath, headers, {}, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
