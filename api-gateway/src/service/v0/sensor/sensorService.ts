378b6e97f6b717233206784b287fdf8dc38089cc
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-31 12:12:38 +0100

    refactor: use new header type

--- api-gateway/src/service/v0/sensor/sensorService.ts
+++ api-gateway/src/service/v0/sensor/sensorService.ts
@@ -13,25 +13,25 @@ interface SensorOperations {
     /**
      * Register a new input sensor.
      * @param {string} _endpointPath - Endpoint path where the request will be redirected;
-     * @param {Record<string, string>} _headers - Input headers that will be redirected to the sensor registry;
+     * @param {Record<string, string | undefined | string[]>} _headers - Input headers that will be redirected to the sensor registry;
      * @param {object} _body - Input sensor information;
      * @returns {Promise<HttpResponse>} Sensor registry response.
      */
-    registerOperation(_endpointPath: string, _headers: Record<string, string>, _body: object): Promise<HttpResponse>;
+    registerOperation(_endpointPath: string, _headers:Record<string, string | undefined | string[]>, _body: object): Promise<HttpResponse>;
     /**
      * Return all the different stored sensors;
      * @param {string} _endpointPath - Endpoint path where the request will be redirected;
-     * @param {Record<string, string>} _headers - Input headers that will be redirected to the sensor registry;
+     * @param {Record<string, string | undefined | string[]> _headers - Input headers that will be redirected to the sensor registry;
      * @returns {Promise<HttpResponse>} Sensor registry response.
      */
-    getAllSensorsOperation(_endpointPath: string, _headers: Record<string, string>): Promise<HttpResponse>;
+    getAllSensorsOperation(_endpointPath: string, _headers: Record<string, string | undefined | string[]>): Promise<HttpResponse>;
     /**
      * Delete the input sensor.
      * @param {string} _endpointPath - Endpoint path where the request will be redirected;
-     * @param {Record<string, string>} _headers - Input headers;
+     * @param {RRecord<string, string | undefined | string[]>} _headers - Input headers;
      * @returns {Promise<HttpResponse>} Sensor registry response.
      */
-    deleteOperation(_endpointPath: string, _headers: Record<string, string>): Promise<HttpResponse>;
+    deleteOperation(_endpointPath: string, _headers: Record<string, string | undefined | string[]>): Promise<HttpResponse>;
     /**
      * Update the input sensor.
      * @param {string} _endpointPath - Endpoint path where the request will be redirected.
@@ -47,22 +47,22 @@ class SensorService<T extends HttpClient> extends AbstractService<T> implements 
         super(circuitBreaker, endpoint, authenticationClient);
     }
 
-    registerOperation(endpointPath: string, headers: any, body: object): Promise<HttpResponse> {
+    registerOperation(endpointPath: string, headers: Record<string, string | undefined | string[]>, body: object): Promise<HttpResponse> {
         const request = new BasicHttpRequest(HttpMethods.POST, endpointPath, headers, body, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
-    getAllSensorsOperation(endpointPath: string, headers: any): Promise<HttpResponse> {
+    getAllSensorsOperation(endpointPath: string, headers: Record<string, string | undefined | string[]>): Promise<HttpResponse> {
         const request = new BasicHttpRequest(HttpMethods.GET, endpointPath, headers, {}, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
-    deleteOperation(endpointPath: string, headers: any): Promise<HttpResponse> {
+    deleteOperation(endpointPath: string, headers: Record<string, string | undefined | string[]>,): Promise<HttpResponse> {
         const request = new BasicHttpRequest(HttpMethods.DELETE, endpointPath, headers, {}, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
 
-    updateRemoteSensor(endpointPath: string, headers: any, body: any): Promise<HttpResponse> {
+    updateRemoteSensor(endpointPath: string, headers: Record<string, string | undefined | string[]>, body: any): Promise<HttpResponse> {
         const request = new BasicHttpRequest(HttpMethods.PUT, endpointPath, headers, body, {}, {});
         return this.circuitBreaker.fireRequest(this.endpoint, request);
     }
