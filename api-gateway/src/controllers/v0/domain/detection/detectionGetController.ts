b1f174eca91f4b3e60a429c1ddf69dbc5ca15fb8
Fabio Vincenzi <fabio.vincenzi2001@gmail.com>
2025-02-05 10:55:15 +0100

    fix: replace detection endpoint with sensor

--- api-gateway/src/controllers/v0/domain/detection/detectionGetController.ts
+++ api-gateway/src/controllers/v0/domain/detection/detectionGetController.ts
@@ -5,12 +5,23 @@ import Logger from 'js-logger';
 import HttpStatus from 'http-status-codes';
 import { fromHttpResponseToExpressResponse } from '../../utils/api/responseUtils';
 import { detectionService } from './detectionConfig';
+import { USER_TOKEN_HEADER } from '../../../../models/v0/authentication/headers/authenticationHeaders';
 
 Logger.useDefaults();
 
 const detectionGetHandler = async (request: Request, response: Response) => {
     let endpointPath = removeServiceFromUrl(DETECTION_ENDPOINT, request.url);
+    endpointPath = endpointPath.replace('detection', 'sensor');
+    console.log(endpointPath);
             try {
+                const token = String(request.headers[USER_TOKEN_HEADER.toLowerCase()]) || '';
+                const isExpired = await detectionService.authenticationClient.isExpired(token);
+
+                if (isExpired) {
+                    Logger.info('The token is expired');
+                    response.status(HttpStatus.UNAUTHORIZED);
+                    return;
+                }
                 const httpResponse = await detectionService.getDetectionsOperation(
                     endpointPath,
                     request.headers,
