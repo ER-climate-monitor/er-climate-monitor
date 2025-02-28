66bb7ba16f3dc6f7fec8e35e9b2f2c19055ce6c0
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-02-28 10:08:16 +0100

    doc: add jsodc to url utility file

--- api-gateway/src/controllers/v0/utils/api/urlUtils.ts
+++ api-gateway/src/controllers/v0/utils/api/urlUtils.ts
@@ -1,3 +1,9 @@
+/**
+ * URL utility for removing the input service from the input url.
+ * @param {string} service - Input service to remove from the url.
+ * @param {string} url - Input url.
+ * @returns {string} the new url without having the input service inside it.
+ */
 function removeServiceFromUrl(service: string, url: string): string {
     return url.replace(service, '');
 }
