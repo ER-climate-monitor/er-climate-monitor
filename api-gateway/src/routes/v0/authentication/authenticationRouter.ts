c918815e8430cc9b3cd91a9fab9a80f6efffe698
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-02-28 10:14:13 +0100

    doc: add jsdoc to Authentication router

--- api-gateway/src/routes/v0/authentication/authenticationRouter.ts
+++ api-gateway/src/routes/v0/authentication/authenticationRouter.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { AUTHENTICATION_PATHS } from '../paths/gatewayPaths';
 import { authenticationPostHandler } from '../../../controllers/v0/domain/authentication/authenticationPostController';
 import { authenticationDeleteHandler } from '../../../controllers/v0/domain/authentication/authenticationDeleteController';
-
+/**
+ * Authentication router that manages all the incoming requests towards the authentication service.
+ */
 const authenticationRouter = express.Router();
 
 authenticationRouter.route(AUTHENTICATION_PATHS).post(authenticationPostHandler).delete(authenticationDeleteHandler);
