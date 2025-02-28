ecf81d5472a1439603670e67b52c36dd3ad43550
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-02-28 10:00:49 +0100

    doc: add jsdoc to response utils

--- api-gateway/src/controllers/v0/utils/api/responseUtils.ts
+++ api-gateway/src/controllers/v0/utils/api/responseUtils.ts
@@ -1,6 +1,12 @@
 import { Response } from 'express';
 import { HttpResponse } from '../circuitBreaker/http/httpResponse';
 
+/**
+ * Convert the input HttpResponse into the Express Response.
+ * @param {HttpResponse} httpResponse - Input Http Response to convert
+ * @param {Response} response - Output response where all the information will be saved.
+ * @returns {Response} the new response to return.
+ */
 function fromHttpResponseToExpressResponse(httpResponse: HttpResponse, response: Response): Response {
     for (const header in httpResponse.headers) {
         response.setHeader(header, httpResponse.headers[header]);
@@ -9,6 +15,12 @@ function fromHttpResponseToExpressResponse(httpResponse: HttpResponse, response:
     return response;
 }
 
+/**
+ * Handle Http Response that contains all the error information, this response will be converted into a Express Response.
+ * @param {HttpResponse} error - HttpResponse containing the error.
+ * @param {Response} response - Output response where all the information will be saved.
+ * @returns {Response} the new response to return.
+ */
 function handleError(error: HttpResponse, response: Response) {
     response = fromHttpResponseToExpressResponse(error, response);
     response.send(error.data);
