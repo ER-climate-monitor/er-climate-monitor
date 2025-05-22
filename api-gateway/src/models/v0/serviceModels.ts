e4bc31197a3c2e3bfd1e75a1cb3696eb6aa42a6a
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-22 15:43:11 +0200

    feat(url): update urls with deployed one

--- api-gateway/src/models/v0/serviceModels.ts
+++ api-gateway/src/models/v0/serviceModels.ts
@@ -3,11 +3,11 @@ import { NOTIFICATIONS_API } from '../../routes/v0/paths/gatewayPaths';
 
 config();
 
-const AUTHENTICATION_ENDPOINT = 'http://localhost:8888/v0';
-const SENSOR_REGISTRY_ENDPOINT = 'http://localhost:7777/v0';
-const DETECTION_ENDPOINT = 'http://localhost:8887/v0';
-const NOTIFICATION_ENDPOINT = `http://${NOTIFICATIONS_API.SERVICE.HOSTNAME}:${NOTIFICATIONS_API.SERVICE.PORT}/v0`;
-const DETECTION_SOCKET_ENDPOINT = 'http://localhost:8886/';
+const AUTHENTICATION_ENDPOINT = 'https://authentication-service-17633123551.europe-west8.run.app/v0'//'http://localhost:8888/v0';
+const SENSOR_REGISTRY_ENDPOINT = 'https://sensor-registry-17633123551.europe-west8.run.app/v0';// 'http://localhost:7777/v0';
+const DETECTION_ENDPOINT = 'https://detection-service-17633123551.europe-west8.run.app/v0'// 'http://localhost:8887/v0';
+const NOTIFICATION_ENDPOINT = 'https://notification-service-17633123551.europe-west8.run.app/v0'//`http://${NOTIFICATIONS_API.SERVICE.HOSTNAME}:${NOTIFICATIONS_API.SERVICE.PORT}/v0`;
+const DETECTION_SOCKET_ENDPOINT = 'https://detection-service-17633123551.europe-west8.run.app'// 'http://localhost:8886/';
 
 export {
     AUTHENTICATION_ENDPOINT,
