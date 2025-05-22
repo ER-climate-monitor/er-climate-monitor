42870c7426f675430e833bd15823ecd692b594e7
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-22 12:57:44 +0200

    refactor: update db url

--- authentication-service/src/index.ts
+++ authentication-service/src/index.ts
@@ -1,7 +1,7 @@
 import { createProdServer } from './appUtils';
 
 const PORT = process.env.PORT || 3000;
-const URL: string = process.env.DB_URL || 'mongodb://localhost:27017';
+const URL: string = process.env.DB_URL || "mongodb+srv://matteoiorio01:bsHMrwnluMpMptJY@cluster0.tj3ou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = createProdServer(URL);
 
