00918ea9ecc8cc5e682008de6e0b588c36f4a439
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-23 09:57:42 +0200

    feat(cors): allow any origin

--- detection-service/src/sockets/socket.ts
+++ detection-service/src/sockets/socket.ts
@@ -7,7 +7,7 @@ let io: Server;
 export const setupSocketServer = (server: http.Server) => {
     io = new Server(server, {
         cors: {
-            origin: 'http://localhost:5173',
+            origin: '*',
             methods: ['GET', 'POST'],
         },
     });
@@ -20,7 +20,7 @@ export const setupSocketServer = (server: http.Server) => {
             socket.join(sensorId);
         });
 
-        socket.on('unsubscribe', (sensorId: string) => {
+          socket.on('unsubscribe', (sensorId: string) => {
             console.log(`Client ${socket.id} unsubscribed from sensor ${sensorId}`);
             socket.leave(sensorId);
         });
